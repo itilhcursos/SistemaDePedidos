5341afa0e4a74ddb7ab001e358755573a4ff7074
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-16 14:19:00 -0300

    UPDATE: Novo título e novo TheWelcome.vue.

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -8,24 +8,56 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
+<div class="greetings">
+  <header>
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      Bem vindo a sua lista de compras on-line.
-    </h3>
-  </div>
+    <h2>
+      Bem vindo(a) a sua lista de compras online. Este é um projeto de lista de compras online com <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> e com <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue3</a>.
+    </h2>
+  </header>
+</div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+
+:root {
+  --cor0: #c8d1f2;
+  --cor1: #0B1654;
+  --cor2: #33395D;
+  --cor3: #8591D4;
+  --cor4: #AE91D4;
+  --cor5: #d65f71;
+  
+  --fonte-padrao: Arial, Verdana, Helvetica, sans-serif;
+  --fonte-destaque: 'Bebas Neue', sans-serif;
 }
 
-h3 {
-  font-size: 1.2rem;
+* {
+  padding: 0px;
+  margin: 0px;
+}
+
+header {
+  background-color: #d65f71;
+  padding-top: 30px;
+  padding-bottom: 20px;
+  min-height: 80px;
+  text-align: center;
+}
+
+ h1 {
+  margin-bottom: 20px;
+  text-shadow: 2px 2px 0px rgba(0, 0, 0, 0.267);
+  font-family: var(--fonte-destaque);
+  font-weight: normal;
+  font-size: 3em;
+  color: #0B1654;
+}
+
+h2 {
+  text-shadow: 2px 2px 0px rgba(0, 0, 0, 0.267);
+  font-size: 15pt;
+  color: #0B1654;
 }
 
 .greetings h1,
@@ -33,10 +65,4 @@ h3 {
   text-align: center;
 }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,7 +4,22 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <h1>Teste de Alteração 02</h1>
+    <h3>Documentação Adicional</h3>
     <TheWelcome />
   </main>
 </template>
+
+<style scoped>
+
+h3 {
+  background-color: #33395D;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  border-radius: 5px;
+  text-shadow: 2px 2px 0px rgba(0, 0, 0, 0.267);
+  text-align: left;
+  padding: 10px;
+  color: #d65f71;
+}
+
+</style>
