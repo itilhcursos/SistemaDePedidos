131ed2f14691bd5f2fa4379ded0768f7307fd768
willianxzy <willianlittig14@gmail.com>
2024-07-16 22:36:55 -0300

    UPDATE: colocado texto "sobre" com informações do projeto + estilizações

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <h1>texto que eu quero!!!</h1>
+    <h1>O Sistema de Compras é uma aplicação web desenvolvida para otimizar o
+        processo de compras dos cidadãos. <br>
+        Projetado inicialmente para facilitar a anotação de produtos e suas respectivas quantidades,
+        o sistema visa otimizar o tempo na hora das compras, não ocorrer de esquecer algum produto e melhorar o controle sobre o estoque e as despesas dentro de casa.</h1>
   </div>
 </template>
 
@@ -10,6 +13,11 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+  }
+
+  .about > h1 {
+    font-size: 25px;
+    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   }
 }
 </style>
