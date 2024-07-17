c588a9ff72c94204fd274c49b895599ee93c02b4
willianxzy <willianlittig14@gmail.com>
2024-07-16 22:35:47 -0300

    UPDATE: refatorando page home com informações do sistema.

--- frontend/src/components/TheWelcome.vue
+++ frontend/src/components/TheWelcome.vue
@@ -14,73 +14,44 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Orientações</template>
 
-    Bem-vindo a lista de compras.....
+    Bem-vindo(a) à lista de compras... <br>
+    Abaixo temos algumas tecnologias que foram utilizadas no projeto e algumas outras orientações. <br>
+    Do lado esquerdo é possível navegar entre as páginas e explorar ainda mais as funcionalidades desse projeto. xD
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>Ferramentas</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
+    Este projeto tem como sua principal linguagem o
+    <a href="https://adoptium.net/download/" target="_blank" rel="noopener">Java</a>, utilizada no backend do sistema.
+    <br>
+    Os Frameworks e Bibliotecas utilizados: 
+    <a href="https://spring.io/projects/spring-boot" target="_blank" rel="noopener">Spring Boot</a>, 
+    <a href="https://spring.io/projects/spring-data-jpa" target="_blank" rel="noopener">Spring Data JPA</a> e 
+    <a href="https://getbootstrap.com/" target="_blank" rel="noopener">Bootstrap</a>.
+    <br>
+    Como banco de dados utilizamos o 
+    <a href="https://www.postgresql.org/download/" target="_blank" rel="noopener">PostgreSQL</a>.
     <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    Utilizado como ferramenta de build e gerenciamento de dependências utilizamos o 
+    <a href="https://maven.apache.org/download.cgi" target="_blank" rel="noopener">Maven</a>.
+    <br>
+    Para realizar o controle de versão estamos utilizando o 
+    <a href="https://git-scm.com/download/win" target="_blank" rel="noopener">Git</a> e o 
+    <a href="https://github.com/" target="_blank" rel="noopener">GitHub</a>.
+    <br>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Community</template>
+    <template #heading>Comunidade</template>
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    Não temos nenhuma comunidade pública atualmente, apenas o repositório do projeto no
+    <a href="https://github.com/itilhcursos/SistemaDePedidos" target="_blank" rel="noopener">GitHub</a>
   </WelcomeItem>
 </template>
