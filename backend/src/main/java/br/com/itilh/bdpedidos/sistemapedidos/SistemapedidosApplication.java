6dd8b9206f4d1737b284533d2e3b21abc7575a96
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-19 21:19:34 -0300

    UPDATE: SistemapedidosApplication atualizado.

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplication.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplication.java
@@ -25,7 +25,7 @@ public class SistemapedidosApplication {
             }
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("*");
             }
 
         };
