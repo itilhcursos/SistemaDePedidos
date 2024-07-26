331b4d897f9ee1ff81498a8fa3455ba9d306529a
itilhcursos <itilhcursos@gmail.com>
2024-07-26 17:50:38 -0300

    UPDATE: Versão inicial do controle de paginação e ordenação

--- frontend/src/components/ListarEstado.vue
+++ frontend/src/components/ListarEstado.vue
@@ -2,10 +2,12 @@
   <div class="container">
     <div class="row">
       <div class="col-10">
-        <h3 >ESTADOS</h3>
+        <h3>ESTADOS</h3>
       </div>
       <div class="col-2 d-flex justify-content-end">
-        <button v-if="!formVisible" @click="novoEstado" class="btn btn-success"><i class="bi bi-clipboard-plus"></i> Novo</button>
+        <button v-if="!formVisible" @click="novoEstado" class="btn btn-success">
+          <i class="bi bi-clipboard-plus"></i> Novo
+        </button>
       </div>
       <div class="row">
         <div>
@@ -36,9 +38,19 @@
             {{ estado.nome }}
           </td>
           <td class="d-flex justify-content-end">
-            <button class="btn btn-btn btn-primary  m-2" @click="alterarEstado(estado)"><i class="bi bi-clipboard-pulse"></i> Alterar</button>
+            <button
+              class="btn btn-btn btn-primary m-2"
+              @click="alterarEstado(estado)"
+            >
+              <i class="bi bi-clipboard-pulse"></i> Alterar
+            </button>
 
-            <button class="btn btn-outline-danger m-2" @click="excluirEstado(estado.id)"><i class="bi bi-clipboard2-minus"></i> Excluir</button>
+            <button
+              class="btn btn-outline-danger m-2"
+              @click="excluirEstado(estado.id)"
+            >
+              <i class="bi bi-clipboard2-minus"></i> Excluir
+            </button>
           </td>
         </tr>
       </tbody>
@@ -46,11 +58,53 @@
   </div>
   <div v-if="!formVisible">
     <hr />
-    <h2>Paginação</h2>
-    <p>
-      <input type="text" v-model="pageNumber" placeholder="Número da pagina" />
-    </p>
-    <p><button @click.prevent="buscarEstados">Buscar</button></p>
+    <div class="row d-flex justify-content-center">
+      <div class="col-auto">
+        <div>
+          <button
+            v-for="pagina in totalPages"
+            :key="pagina"
+            @click.prevent="irPara(pagina)"
+            class="btn btn-secondary ms-1 "
+          >
+            {{ pagina }}
+          </button>
+        </div>
+      </div>
+      <div class="col-auto">
+        <input
+            class="form-control"
+            type="text"
+            v-model="pageNumber"
+          />
+      </div>
+      <div class="col-auto">
+        <select class="form-select" aria-label="Propriedade" v-model="pageSize">
+          <option selected value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+          <option value="1">1</option>
+        </select>
+      </div>
+      <div class="col-auto">
+        <select class="form-select" aria-label="Propriedade" v-model="property">
+          <option selected value="id">ID</option>
+          <option value="nome">Nome</option>
+        </select>
+      </div>
+      <div class="col-auto">
+        <select class="form-select" aria-label="Direção" v-model="direction">
+          <option selected value="ASC">Crescente</option>
+          <option value="DESC">Decrescente</option>
+        </select>
+      </div>
+      <div class="col-auto">
+        <button @click.prevent="buscarEstados" class="btn btn-success">
+          Buscar
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -69,7 +123,14 @@ export default {
       formVisible: false,
       mode: import.meta.env.MODE,
       url: import.meta.env.VITE_APP_URL_API,
+
       pageNumber: 1,
+      pageSize: 10,
+      direction: "ASC",
+      property: "nome",
+      totalPages: 0,
+
+      pageable: null,
     };
   },
   methods: {
@@ -79,10 +140,13 @@ export default {
       //buscar a lista de estados no servidor
       // http://localhost:8080/estados
       const response = await axios.get(
-        `http://localhost:8080/estados?pageNumber=${this.pageNumber}`
+        `http://localhost:8080/estados?pageNumber=${this.pageNumber}&pageSize=${this.pageSize}&direction=${this.direction}&property=${this.property}`
       );
       console.log(response.data);
       this.listaEstados = response.data.content;
+      this.pageable = response.data.pageable;
+      this.totalPages = response.data.totalPages;
+      
     },
     limpar() {
       this.estadoEscolhido = null;
@@ -100,6 +164,10 @@ export default {
       console.log(response.data);
       this.buscarEstados();
     },
+    irPara(numero){
+      this.pageNumber = numero;
+      this.buscarEstados();
+    }
   },
   mounted() {
     this.buscarEstados();
