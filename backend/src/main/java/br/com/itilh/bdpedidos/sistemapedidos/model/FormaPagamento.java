d29aaafba0d9eb57c70db1655ac1fe937e580a2b
itilhcursos <itilhcursos@gmail.com>
2024-07-26 13:56:59 -0300

    UPDATE: inclusões no backEnd para atividade avaliativa

---
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/model/FormaPagamento.java
@@ -0,0 +1,37 @@
+package br.com.itilh.bdpedidos.sistemapedidos.model;
+
+import java.math.BigInteger;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "tb_formas_pagamento")
+
+public class FormaPagamento {
+    @Id
+    @SequenceGenerator(name = "sequencial", sequenceName = "tb_formas_pagamento_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequencial")
+    private BigInteger id;
+
+    @Column(name = "tx_descricao")
+    private String descricao;
+
+    @Column(name = "bo_entrega")
+    private Boolean entrega;
+}
