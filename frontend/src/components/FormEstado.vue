cb48b70db3545cf10d64fe59a4462ae396586d9c
FabianoNovelli <fabiano.novelli@hotmail.com>
2024-07-26 21:48:17 -0300

    UPDATE: Branch desafio de design autor: Fabiano Novelli

--- frontend/src/components/FormEstado.vue
+++ frontend/src/components/FormEstado.vue
@@ -1,68 +1,118 @@
 <template>
-    <div>
-        <h1>Cadastro de estado</h1>
-        <hr/>
-        <p><input type="text" v-model="id" :disabled="true" placeholder="Id estado" /></p>
-        <p><input type="text" v-model="nome" placeholder="Nome" /></p>
-        <div v-if="isInvalido"  role="alert">
-            Nome deve ser preenchido!!
-        </div>
-        <button type="submit" v-on:click.prevent="salvarEstado"  >Incluir</button>
-        <button type="submit" v-on:click.prevent="cancelar" >Cancelar</button>
-        <hr/>
-    </div>
+  <div class="container">
+    <h4 class="p-1 mb-1 bg-success text-white">{{ getAcao }} Estado</h4>
+    <hr />
+    <form>
+      <div class="mb-3">
+        <label class="form-label">Id</label>
+        <input
+          class="form-control"
+          type="text"
+          v-model="id"
+          :disabled="true"
+          placeholder="Id estado"
+        />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Nome</label>
+        <input
+          class="form-control"
+          type="text"
+          v-model="nome"
+          placeholder="Nome"
+        />
+      </div>
+      <div v-if="isInvalido" class="alert alert-danger d-flex align-items-center" role="alert">
+        <i class="bi bi-exclamation-triangle-fill"></i>
+        <div class="p-2">Nome deve ser preenchido!!</div>
+      </div>
+      <div class="mb-3 d-flex justify-content-end">
+        <button
+          class="btn btn-primary m-2"
+          type="submit"
+          v-on:click.prevent="salvarEstado"
+        >
+        <i class="bi bi-clipboard2-check"></i>
+          {{ getAcao }}
+        </button>
+        <button
+          class="btn btn-warning m-2"
+          type="submit"
+          v-on:click.prevent="cancelar"
+        >
+        <i class="bi bi-clipboard2-x"></i>
+          Cancelar
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
-<script> 
+<script>
 import axios from "axios";
-   export default{
-        props:{
-            estado: Object
-        },
-        data(){
-            return {
-                id:'',
-                nome:'',
-                isInvalido:false,
-            }
-        },
-        methods:{
-            async salvarEstado(){
-                if(this.nome ==='' ){
-                    this.isInvalido = true;
-                    return;
-                }
-                this.isInvalido = false;
+export default {
+  props: {
+    propsEstado: Object,
+  },
+  data() {
+    return {
+      id: "",
+      nome: "",
+      isInvalido: false,
+    };
+  },
+  methods: {
+    async salvarEstado() {
+      if (this.nome === "") {
+        this.isInvalido = true;
+        return;
+      }
+      this.isInvalido = false;
 
-                const response = await axios.post("http://localhost:8080/estado", {
-                         id:this.id,
-                         nome:this.nome
-                     });
-                console.log(response.data);
-                this.listaEstados = response.data;
+      if (this.id === "") {
+        //incluir pelo POST da API
+        const response = await axios.post("http://localhost:8080/estado", {
+          id: this.id,
+          nome: this.nome,
+        });
+        this.listaEstados = response.data;
+      } else {
+        // alterar pelo PUT da API
+        const response = await axios.put(
+          `http://localhost:8080/estado/${this.id}`,
+          {
+            id: this.id,
+            nome: this.nome,
+          }
+        );
+        this.listaEstados = response.data;
+      }
 
-                 this.$emit('salvar_estado',{
-                         id:this.id,
-                         nome:this.nome
-                     });
+      this.$emit("salvar_estado", {
+        id: this.id,
+        nome: this.nome,
+      });
 
-                this.id = '';
-                this.nome ='';
-            
-            },
-            cancelar(){
-                this.id = '';
-                this.nome ='';
-                this.$emit('cancelar',true);
-            
-            },
-        },
-        computed() {
-            if(this.estado){
-                this.id =this.estado.id;
-                this.nome =this.estado.nome;
-            }
-        }
-   }
+      this.id = "";
+      this.nome = "";
+    },
+    cancelar() {
+      this.id = "";
+      this.nome = "";
+      this.$emit("cancelar", true);
+    },
+  },
+  mounted() {
+    if (this.propsEstado) {
+      this.id = this.propsEstado.id;
+      this.nome = this.propsEstado.nome;
+    }
+  },
+  computed: {
+    getAcao() {
+      return this.id === "" ? "Incluir" : "Alterar";
+    },
+  },
+};
 </script>
 
--- frontend/src/components/ListarEstado.vue
+++ frontend/src/components/ListarEstado.vue
@@ -1,88 +1,156 @@
 <template>
-    <div>
-        <p>Lista de Estados</p>
-        <button v-if="!formVisible" @click="novoEstado" class="btn btn-primary">Novo</button>
-        <p></p>
-       <FormEstado v-if="formVisible" @cancelar="limpar" @salvar_estado="buscarEstados"/>
-        <table class="tb_estado">
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Ações</th>
-            </tr>
-            <tr v-for="estado in listaEstados" :key="estado.id">
-                <td>
-                    {{ estado.id }}
-                </td>
-                <td>
-                    {{ estado.nome }}
-                </td>
-                <td>
-                    <button @click="alterarEstado(estado)">Alterar</button>
-                    <button @click="excluirEstado(estado.id)">Excluir</button>
-                </td>
-            </tr>
-        </table>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+        <h3 >ESTADOS</h3>
+      </div>
+      <div class="col-2 d-flex justify-content-end">
+        <button v-if="!formVisible" @click="novoEstado" class="btn btn-success"><i class="bi bi-clipboard-plus"></i> Novo</button>
+      </div>
+      <div class="row">
+        <div>
+          <FormEstado
+            v-if="formVisible"
+            :propsEstado="estadoEscolhido"
+            @cancelar="limpar"
+            @salvar_estado="buscarEstados"
+          />
+        </div>
+      </div>
     </div>
+
+    <table class="table table-dark table-striped" v-if="!formVisible">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nome</th>
+          <th scope="col" class="d-flex justify-content-end">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="estado in listaEstados" :key="estado.id" scope="row">
+          <th>
+            {{ estado.id }}
+          </th>
+          <td>
+            {{ estado.nome }}
+          </td>
+          <td class="d-flex justify-content-end">
+            <button class="btn btn-btn btn-primary  m-2" @click="alterarEstado(estado)"><i class="bi bi-clipboard-pulse"></i> Alterar</button>
+
+            <button class="btn btn-outline-danger m-2" @click="excluirEstado(estado.id)"><i class="bi bi-clipboard2-minus"></i> Excluir</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div v-if="!formVisible">
+    <hr />
+    
+    <div class="container">
+      <div class="row">
+        <div class="col">1 of 6</div>
+        <div class="col">2 of 6</div>
+        <div class="col">3 of 6</div>
+        <div class="col"><select v-model="property" class="form-select">
+                           <option value="id">ID</option>
+                           <option value="nome">Nome</option>      
+                         </select>
+         </div>
+        <div class="col"><select v-model="direction" class="form-select">
+                           <option value="ASC">Crescente</option>
+                           <option value="DESC">Decrescente</option>      
+                         </select>
+        </div>
+
+        <div class="col"><button @click.prevent="buscarEstados" class="btn btn-success" ><i class="bi bi-binoculars"></i>Buscar</button></div>
+      </div>
+    </div>
+
+    <h2>Paginação</h2>
+
+    <p><button v-for="pagina in totalPages" :key="pagina" @click.prevent="irPara(pagina)" >{{  pagina }}</button></p>
+
+    <p><input type="text" v-model="pageNumber" placeholder="Número da pagina" /></p>
+
+    <select v-model="direction">
+      <option value="ASC">Crescente</option>
+      <option value="DESC">Decrescente</option>      
+    </select>
+
+    
+
+    <select v-model="pageSize">
+      <option value="2">2</option>
+      <option value="10">10</option>
+      <option value="20">20</option>      
+      <option value="50">50</option>            
+    </select>
+
+    <p></p>
+  </div>
 </template>
 
 
-<script> 
-import FormEstado from './FormEstado.vue';
+<script>
+import FormEstado from "./FormEstado.vue";
 import axios from "axios";
-    export default {
-        components:{
-            FormEstado
-        },
-        data(){
-            return{
-                listaEstados:[],
-                formVisible:false
-            }
-        },
-        methods:{
-            async buscarEstados(){
-                this.formVisible = false;
-                //buscar a lista de estados no servidor
-                // http://localhost:8080/estados 
-                const response = await axios.get("http://localhost:8080/estados");
-                console.log(response.data);
-                this.listaEstados = response.data;
-            },
-            limpar(){
-                this.formVisible = !this.formVisible;
-            }, 
-            novoEstado(){
-                this.formVisible = !this.formVisible;
-            },
-            alterarEstado(estado){
-                console.log(estado);
-            },
-            async excluirEstado(id){
-                const response = await axios.delete(`http://localhost:8080/estado/${id}`);
-                console.log(response.data);
-                this.buscarEstados();
-            }
-        } ,  
-        mounted() {
-            this.buscarEstados();
-        }     
-    }
-</script>
+export default {
+  components: {
+    FormEstado,
+  },
+  data() {
+    return {
+      listaEstados: [],
+      estadoEscolhido: null,
+      formVisible: false,
+      mode: import.meta.env.MODE,
+      url: import.meta.env.VITE_APP_URL_API,
+      pageNumber: 1,
+      pageSize: 10,
+      direction: 'ASC',
+      property: 'id',
+      totalPages:0
+    };
+  },
+  methods: {
+    async buscarEstados() {
+      this.estadoEscolhido = null;
+      this.formVisible = false;
+      //buscar a lista de estados no servidor
+      // http://localhost:8080/estados
+      const response = await axios.get(
+        `http://localhost:8080/estados?pageNumber=${this.pageNumber}&pageSize=${this.pageSize}&direction=${this.direction}&property=${this.property}`
+      );
+      console.log(response.data);
+      this.listaEstados = response.data.content;
+      this.totalPages = response.data.totalPages;
+    },
+    limpar() {
+      this.estadoEscolhido = null;
+      this.formVisible = !this.formVisible;
+    },
+    novoEstado() {
+      this.formVisible = !this.formVisible;
+    },
+    alterarEstado(estado) {
+      this.estadoEscolhido = estado;
+      this.formVisible = true;
+    },
+    async excluirEstado(id) {
+      const response = await axios.delete(`http://localhost:8080/estado/${id}`);
+      console.log(response.data);
+      this.buscarEstados();
+    },
 
-<style scoped>
-  table.tb_estado {
-    border-collapse: collapse; /* CSS2 */
-    background: #FFFFF0;
-}
- 
-table.tb_estado td {
-    border: 1px solid black;
-}
- 
-table.tb_estado th {
-    border: 1px solid black;
-    background: #F0FFF0;
-}
-</style>
+    irPara(pagina){
+      this.pageNumber = pagina;
+      this.buscarEstados();
+    },
+
+  },
+  mounted() {
+    this.buscarEstados();
+  },
+};
+</script>
