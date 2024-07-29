ff86e48d242d5b10530763b87b47daf988c1d977
FRCamilo <felipecami11@gmail.com>
2024-07-29 11:49:07 -0300

    Criação do branch

--- frontend/src/components/ListarEstado.vue
+++ frontend/src/components/ListarEstado.vue
@@ -46,14 +46,47 @@
   </div>
   <div v-if="!formVisible">
     <hr />
+
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-auto">
+            <button v-for="pagina in totalPages" :key="pagina" @click.prevent="irPara(pagina)" class="btn btn-light me-1">
+            {{ pagina }}
+            </button>
+        </div>
+        <div class="col-auto"><input type="text" v-model="pageNumber" placeholder="Número da pagina" class="form-control w-25"/></div>
+        <div class="col-auto">
+            <select v-model="pageSize" class="form-select">
+            <option value="2">2</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+            </select>
+        </div>
+        <div class="col-auto">
+            <select class="form-select">
+            <option value="id">ID</option>
+            <option value="nome">Nome</option>
+            </select>
+        </div>
+        <div class="col-auto">
+            <select v-model="direction" class="form-select">
+            <option value="ASC">Crescente</option>
+            <option value="DESC">Decrescente</option>
+            </select>
+        </div>
+        <div class="col-auto">
+            <button @click.prevent="buscarEstados" class="btn btn-success"> 
+            <i class="bi bi-binoculars"></i> Buscar
+            </button>
+        </div>
+      </div>
+    </div>
+
     <h2>Paginação</h2>
-    <p>
-      <input type="text" v-model="pageNumber" placeholder="Número da pagina" />
-    </p>
-    <p><button @click.prevent="buscarEstados">Buscar</button></p>
+    
   </div>
 </template>
-
 
 <script>
 import FormEstado from "./FormEstado.vue";
@@ -70,6 +103,10 @@ export default {
       mode: import.meta.env.MODE,
       url: import.meta.env.VITE_APP_URL_API,
       pageNumber: 1,
+      pageSize: '10',
+      direction: 'ASC',
+      property: 'id',
+      totalPages: 0
     };
   },
   methods: {
@@ -79,10 +116,12 @@ export default {
       //buscar a lista de estados no servidor
       // http://localhost:8080/estados
       const response = await axios.get(
-        `http://localhost:8080/estados?pageNumber=${this.pageNumber}`
+        `http://localhost:8080/estados?pageNumber=${this.pageNumber}&pageSize=${this.pageSize}&direction=${this.direction}&property=${this.property}`
       );
       console.log(response.data);
       this.listaEstados = response.data.content;
+      this.totalPages = response.data.totalPages;
+      console.log(this.totalPages);
     },
     limpar() {
       this.estadoEscolhido = null;
@@ -100,6 +139,10 @@ export default {
       console.log(response.data);
       this.buscarEstados();
     },
+    irPara(pagina){
+        this.pageNumber = pagina;
+        this.buscarEstados();
+    }
   },
   mounted() {
     this.buscarEstados();
