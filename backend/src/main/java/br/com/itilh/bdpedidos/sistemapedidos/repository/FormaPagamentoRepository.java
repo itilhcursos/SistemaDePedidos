728aafef7bf7686434633530896a7031664f0dfe
willianxzy <willianlittig14@gmail.com>
2024-07-30 11:40:13 -0300

    UPDATE: alterações repository *

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
@@ -1,13 +1,21 @@
 package br.com.itilh.bdpedidos.sistemapedidos.repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import br.com.itilh.bdpedidos.sistemapedidos.model.Estado;
 import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
 
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, BigInteger> {
+
+@Override
+Page<FormaPagamento> findAll(Pageable pageable);
 
 }
