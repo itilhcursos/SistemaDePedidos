d027b24012bbfa6d6aeb29d195a8f030af8f341c
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-30 11:06:25 -0300

    UPDATE: Novos Controller e Repository do Produto e Forma de Pagamento.

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
@@ -1,8 +1,12 @@
 package br.com.itilh.bdpedidos.sistemapedidos.repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
@@ -10,4 +14,21 @@ import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, BigInteger> {
 
+    // Select * from tb_formas_pagamento where tx_descricao = 'descricao'//
+    List<FormaPagamento> findByDescricao(String descricao);
+
+    // Select * from tb_formas_pagamento where UPPER(tx_descricao) like UPPER('descricao%')//
+    List<FormaPagamento> findByDescricaoStartingWithIgnoreCase(String descricao);
+
+    // Select * from tb_formas_pagamento where UPPER(tx_descricao) like UPPER('%descricao')//
+    List<FormaPagamento> findByDescricaoEndingWithIgnoreCase(String descricao);
+
+    // Select * from tb_formas_pagamento where UPPER(tx_descricao) like UPPER('%descricao%')//
+    List<FormaPagamento> findByDescricaoContainingIgnoreCase(String descricao);
+
+    @Query("FROM FormaPagamento e WHERE e.descricao like %?1")
+    List<FormaPagamento> findByMinhaQuery(String descricao);
+
+    @Override
+    Page<FormaPagamento> findAll(Pageable pageable);
 }
