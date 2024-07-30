59c3cdb18ac0252cdf449d06fad2446ecc02286e
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-07-29 21:12:41 -0300

    feat: refatoração do código_municipioController & nova_camada_municipioService

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/MunicipioController.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/MunicipioController.java
@@ -3,77 +3,58 @@ package br.com.itilh.bdpedidos.sistemapedidos.controller;
 import java.math.BigInteger;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.itilh.bdpedidos.sistemapedidos.model.Municipio;
-import br.com.itilh.bdpedidos.sistemapedidos.repository.MunicipioRepository;
+import br.com.itilh.bdpedidos.sistemapedidos.service.MunicipioService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
 @RestController
 public class MunicipioController {
-
-    private final MunicipioRepository repositorio;
-
-    public MunicipioController (MunicipioRepository repositorio){
-        this.repositorio = repositorio;
-    }
+    @Autowired
+    private MunicipioService service;
 
     @GetMapping("/municipios")
     public List<Municipio> getMunicipios() {
-        return (List<Municipio>) repositorio.findAll();
+        return service.listarMunicipios();
     }
 
     @GetMapping("/municipios/estado-id/{id}")
     public List<Municipio> getMunicipiosPorEstadoId(@PathVariable BigInteger id) {
-        return (List<Municipio>) repositorio.findByEstadoId(id);
+        return service.listarMunicipiosPorEstadoId(id);
     }
 
     @GetMapping("/municipios/estado-nome/{nome}")
     public List<Municipio> getMunicipiosPorEstadoNome(@PathVariable String nome) {
-        return (List<Municipio>) repositorio.findByEstadoNomeIgnoreCase(nome);
+        return service.listarMunicipiosPorEstadoNome(nome);
     }
-
 
     @GetMapping("/municipio/{id}")
     public Municipio getMunicipioPorId(@PathVariable BigInteger id) throws Exception {
-        return repositorio.findById(id)
-        .orElseThrow(()-> new Exception("Id não encontrado."));
+        return service.buscarMunicipioPorId(id);
     }
 
     @PostMapping("/municipio")
-    public Municipio postMunicipio(@RequestBody Municipio entity) throws Exception {    
-        try{ 
-            return repositorio.save(entity);
-        }catch (Exception ex){
-            throw new Exception("Não foi possível criar o município." + ex.getMessage());
-        }
+    public Municipio postMunicipio(@RequestBody Municipio entity) throws Exception {
+
+        return service.criarMunicipio(entity);
     }
 
     @PutMapping("/municipio/{id}")
     public Municipio putMunicipio(@PathVariable BigInteger id, @RequestBody Municipio entity) throws Exception {
-        try{ 
-            return repositorio.save(entity);
-        }catch (Exception ex){
-            throw new Exception("Não foi possível alterar o município." + ex.getMessage());
-        }
+        return service.alterarMunicipio(id, entity);
     }
 
     @DeleteMapping("/municipio/{id}")
-    public String deleteMunicipio(@PathVariable BigInteger id) throws Exception{
-        try{ 
-             repositorio.deleteById(id);
-             return "Excluído";
-        }catch (Exception ex){
-            throw new Exception("Não foi possível excluir o id informado." + ex.getMessage());
-        }
+    public String deleteMunicipio(@PathVariable BigInteger id) throws Exception {
+        return service.excluirMunicipio(id);
     }
-    
-    
+
 }
