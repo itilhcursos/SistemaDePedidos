19c3f5ff05108b8f7eba64c6981c4c1657cfd98a
willianxzy <willianlittig14@gmail.com>
2024-07-30 21:01:05 -0300

    UPDATE: path formaPagamento

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,6 +19,11 @@ const router = createRouter({
       name: 'estados',
       component:()=>import('../views/EstadosView.vue')
     },
+    {
+      path: '/formas-pagamento',
+      name: 'forma-pagamento',
+      component:()=>import('../views/FormaPagamentoView.vue')
+    },
 
   ]
 })
