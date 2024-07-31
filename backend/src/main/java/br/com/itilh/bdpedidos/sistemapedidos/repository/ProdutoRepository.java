03acad968fff444cb92a842d109d3b35b417788e
Mikael-Lima <mikaeldossantoslima68@gmail.com>
2024-07-31 09:56:19 -0300

    UPDATE: construcão do controller da Classe Produtos e Formas de Pagamento

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ProdutoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ProdutoRepository.java
@@ -1,6 +1,7 @@
 package br.com.itilh.bdpedidos.sistemapedidos.repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,15 @@ import br.com.itilh.bdpedidos.sistemapedidos.model.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, BigInteger> {
+
+    List<Produto> findByDescricao(String descricao);
+
+    List<Produto> findByDescricaoStartingWithIgnoreCase(String descricao);
+
+    List<Produto> findByDescricaoEndingWithIgnoreCase(String descricao);
+
+    List<Produto> findByDescricaoContainingIgnoreCase(String descricao);
+
+    
 
 }
