4fc52f470301dd2742c7d0d94e44e2e98f3ef6fc
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-08-04 17:20:33 -0300

    fix: chamada_comitada

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ProdutoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ProdutoRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import br.com.itilh.bdpedidos.sistemapedidos.model.Produto;
 
@@ -21,8 +21,8 @@ public interface ProdutoRepository extends JpaRepository<Produto, BigInteger> {
 
     List<Produto> findByDescricaoContainingIgnoreCase(String descricao);
 
-    @Query("FROM Produto e WHERE e.descricao like %?1")
-    List<Produto> findByMinhaQuery(String descricao);
+    // @Query("FROM Produto e WHERE e.descricao like %?1")
+    // List<Produto> findByMinhaQuery(String descricao);
     
     @Override
     Page<Produto> findAll(Pageable pageable);
