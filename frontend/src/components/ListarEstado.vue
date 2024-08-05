a53de9fbf4a033de8e921c9f013c72b1a4ce2ac7
itilhcursos <itilhcursos@gmail.com>
2024-08-05 07:51:45 -0300

    Revert "Merge branch 'master' of https://github.com/itilhcursos/SistemaDePedidos into develop"
    This reverts commit 3dd6f96ba0cd599e1bc89ef9bb66bdf72021b78c, reversing
    changes made to 2461e60bbfc4a878f51d571c2806d5ad18d4016a.

--- frontend/src/components/ListarEstado.vue
+++ frontend/src/components/ListarEstado.vue
@@ -2,12 +2,10 @@
   <div class="container">
     <div class="row">
       <div class="col-10">
-        <h3>ESTADOS</h3>
+        <h3 >ESTADOS</h3>
       </div>
       <div class="col-2 d-flex justify-content-end">
-        <button v-if="!formVisible" @click="novoEstado" class="btn btn-success">
-          <i class="bi bi-clipboard-plus"></i> Novo
-        </button>
+        <button v-if="!formVisible" @click="novoEstado" class="btn btn-success"><i class="bi bi-clipboard-plus"></i> Novo</button>
       </div>
       <div class="row">
         <div>
@@ -38,19 +36,9 @@
             {{ estado.nome }}
           </td>
           <td class="d-flex justify-content-end">
-            <button
-              class="btn btn-btn btn-primary m-2"
-              @click="alterarEstado(estado)"
-            >
-              <i class="bi bi-clipboard-pulse"></i> Alterar
-            </button>
+            <button class="btn btn-btn btn-primary  m-2" @click="alterarEstado(estado)"><i class="bi bi-clipboard-pulse"></i> Alterar</button>
 
-            <button
-              class="btn btn-outline-danger m-2"
-              @click="excluirEstado(estado.id)"
-            >
-              <i class="bi bi-clipboard2-minus"></i> Excluir
-            </button>
+            <button class="btn btn-outline-danger m-2" @click="excluirEstado(estado.id)"><i class="bi bi-clipboard2-minus"></i> Excluir</button>
           </td>
         </tr>
       </tbody>
@@ -58,57 +46,11 @@
   </div>
   <div v-if="!formVisible">
     <hr />
-    <div class="container">
-      <div class="row d-flex justify-content-center">
-        <div class="col-auto">
-
-          <button
-            v-for="pagina in totalPages"
-            :key="pagina"
-            @click.prevent="irPara(pagina)"
-            class="btn btn-light ms-1"
-          >
-            {{ pagina }}
-          </button>
-
-
-        </div>
-        <div class="col-auto">
-          <input
-            type="text"
-            v-model="pageNumber"
-            placeholder="Número da pagina"
-            class="form-control w-25"
-          />
-        </div>
-        <div class="col-auto">
-          <select v-model="pageSize" class="form-select">
-            <option value="2">2</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="50">50</option>
-          </select>
-        </div>
-        <div class="col-auto">
-          <select v-model="property" class="form-select">
-            <option value="id">ID</option>
-            <option value="nome">Nome</option>
-          </select>
-        </div>
-        <div class="col-auto">
-          <select v-model="direction" class="form-select">
-            <option value="ASC">Crescente</option>
-            <option value="DESC">Decrescente</option>
-          </select>
-        </div>
-        <div class="col-auto">
-          <button @click.prevent="buscarEstados" class="btn btn-success">
-            <i class="bi bi-binoculars"></i>
-            Buscar
-          </button>
-        </div>
-      </div>
-    </div>
+    <h2>Paginação</h2>
+    <p>
+      <input type="text" v-model="pageNumber" placeholder="Número da pagina" />
+    </p>
+    <p><button @click.prevent="buscarEstados">Buscar</button></p>
   </div>
 </template>
 
@@ -128,10 +70,6 @@ export default {
       mode: import.meta.env.MODE,
       url: import.meta.env.VITE_APP_URL_API,
       pageNumber: 1,
-      pageSize: 10,
-      direction: "ASC",
-      property: "id",
-      totalPages: 0,
     };
   },
   methods: {
@@ -141,12 +79,10 @@ export default {
       //buscar a lista de estados no servidor
       // http://localhost:8080/estados
       const response = await axios.get(
-        `http://localhost:8080/estados?pageNumber=${this.pageNumber}&pageSize=${this.pageSize}&direction=${this.direction}&property=${this.property}`
+        `http://localhost:8080/estados?pageNumber=${this.pageNumber}`
       );
       console.log(response.data);
       this.listaEstados = response.data.content;
-      this.totalPages = response.data.totalPages;
-      console.log(this.totalPages);
     },
     limpar() {
       this.estadoEscolhido = null;
@@ -162,10 +98,6 @@ export default {
     async excluirEstado(id) {
       const response = await axios.delete(`http://localhost:8080/estado/${id}`);
       console.log(response.data);
-      this.buscarEstados();
-    },
-    irPara(pagina) {
-      this.pageNumber = pagina;
       this.buscarEstados();
     },
   },
