9ce16f01557d4dc116eefa23e127804cdca7a09d
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-08-09 14:30:38 -0300

    UPDATE: Refatoração do Municipio e criação de exceções personalizadas.

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/MunicipioController.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/MunicipioController.java
@@ -1,79 +1,88 @@
 package br.com.itilh.bdpedidos.sistemapedidos.controller;
 
 import java.math.BigInteger;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.itilh.bdpedidos.sistemapedidos.model.Municipio;
-import br.com.itilh.bdpedidos.sistemapedidos.repository.MunicipioRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-
-
+import br.com.itilh.bdpedidos.sistemapedidos.dto.MunicipioDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.exception.IdInexistenteException;
+import br.com.itilh.bdpedidos.sistemapedidos.service.MunicipioService;
 
 @RestController
 public class MunicipioController {
 
-    private final MunicipioRepository repositorio;
-
-    public MunicipioController (MunicipioRepository repositorio){
-        this.repositorio = repositorio;
-    }
+    @Autowired
+    private MunicipioService service;
 
     @GetMapping("/municipios")
-    public List<Municipio> getMunicipios() {
-        return (List<Municipio>) repositorio.findAll();
+    public Page<MunicipioDTO> getMunicipios(
+        @RequestParam(required = false, defaultValue = "1") int pageNumber,
+        @RequestParam(required = false, defaultValue = "10") int pageSize,
+        @RequestParam(required = false, defaultValue = "ASC") String direction,
+        @RequestParam(required = false, defaultValue = "id") String property) {
+
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, Sort.Direction.valueOf(direction), property);
+        return service.listarMunicipios(pageable);
     }
 
     @GetMapping("/municipios/estado-id/{id}")
-    public List<Municipio> getMunicipiosPorEstadoId(@PathVariable BigInteger id) {
-        return (List<Municipio>) repositorio.findByEstadoId(id);
+    public Page<MunicipioDTO> getMunicipiosPorEstadoId(@PathVariable BigInteger id,
+        @RequestParam(required = false, defaultValue = "1") int pageNumber,
+        @RequestParam(required = false, defaultValue = "10") int pageSize,
+        @RequestParam(required = false, defaultValue = "ASC") String direction,
+        @RequestParam(required = false, defaultValue = "id") String property
+    ){
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, Sort.Direction.valueOf(direction), property);
+        return service.listarMunicipiosPorEstadoId(id, pageable);
     }
 
     @GetMapping("/municipios/estado-nome/{nome}")
-    public List<Municipio> getMunicipiosPorEstadoNome(@PathVariable String nome) {
-        return (List<Municipio>) repositorio.findByEstadoNomeIgnoreCase(nome);
+    public Page<MunicipioDTO> getMunicipiosPorEstadoNome(@PathVariable String nome,
+        @RequestParam(required = false, defaultValue = "1") int pageNumber,
+        @RequestParam(required = false, defaultValue = "10") int pageSize,
+        @RequestParam(required = false, defaultValue = "ASC") String direction,
+        @RequestParam(required = false, defaultValue = "id") String property
+    ){
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, Sort.Direction.valueOf(direction), property);
+        return service.listarMunicipiosPorEstadoNome(nome, pageable);
     }
 
 
     @GetMapping("/municipio/{id}")
-    public Municipio getMunicipioPorId(@PathVariable BigInteger id) throws Exception {
-        return repositorio.findById(id)
-        .orElseThrow(()-> new Exception("Id não encontrado."));
+    public MunicipioDTO getMunicipioPorId(@PathVariable BigInteger id) throws Exception {
+        try{
+            return service.buscarMunicipioPorId(id);
+        }catch(IdInexistenteException e){
+            //tratar a exceção de forma personalizada
+            throw e;
+        }
+        
     }
 
     @PostMapping("/municipio")
-    public Municipio postMunicipio(@RequestBody Municipio entity) throws Exception {    
-        try{ 
-            return repositorio.save(entity);
-        }catch (Exception ex){
-            throw new Exception("Não foi possível criar o município." + ex.getMessage());
-        }
+    public MunicipioDTO postMunicipio(@RequestBody MunicipioDTO origem) throws Exception {    
+        return service.criarMunicipio(origem);
     }
 
     @PutMapping("/municipio/{id}")
-    public Municipio putMunicipio(@PathVariable BigInteger id, @RequestBody Municipio entity) throws Exception {
-        try{ 
-            return repositorio.save(entity);
-        }catch (Exception ex){
-            throw new Exception("Não foi possível alterar o município." + ex.getMessage());
-        }
+    public MunicipioDTO putMunicipio(@PathVariable BigInteger id, @RequestBody MunicipioDTO origem) throws Exception {
+        return service.alterarMunicipio(id, origem);
     }
 
     @DeleteMapping("/municipio/{id}")
     public String deleteMunicipio(@PathVariable BigInteger id) throws Exception{
-        try{ 
-             repositorio.deleteById(id);
-             return "Excluído";
-        }catch (Exception ex){
-            throw new Exception("Não foi possível excluir o id informado." + ex.getMessage());
-        }
-    }
-    
-    
+        return service.excluirMunicipio(id);
+    } 
 }
---
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/dto/MunicipioDTO.java
@@ -0,0 +1,24 @@
+package br.com.itilh.bdpedidos.sistemapedidos.dto;
+
+import java.math.BigInteger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class MunicipioDTO {
+
+    private BigInteger id;
+    private String nome;
+    private Boolean entrega;
+
+    private BigInteger estadoId;
+    private String estadoNome;
+}
