a0ecf1a39191c808ec40f2059c87415cde01d27d
itilhcursos <itilhcursos@gmail.com>
2024-08-09 17:52:47 -0300

    UPDATE: Mesclando banches em Master

--- backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplicationTests.java
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplicationTests.java
@@ -1,13 +1,17 @@
 package br.com.itilh.bdpedidos.sistemapedidos;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SistemapedidosApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	 void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
+	  }
 
 }
