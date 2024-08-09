8ec70212352df39a3fe3e0f65e433d1d3969b192
Guilherme Vilaruel <guilhermevillaruel@gmail.com>
2024-08-08 21:33:30 -0300

    novas classes produto e forma de pagamento

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
@@ -1,13 +1,26 @@
 package br.com.itilh.bdpedidos.sistemapedidos.repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
 
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, BigInteger> {
+
+    List<FormaPagamento> findByDescricao(String descricao);
+
+    List<FormaPagamento> findByDescricaoStartingWithIgnoreCase(String descricao);
+
+    List<FormaPagamento> findByDescricaoEndingWithIgnoreCase(String descricao);
+
+    List<FormaPagamento> findByDescricaoContainingIgnoreCase(String descricao);
+
+    @Override
+    Page<FormaPagamento> findAll(Pageable pageable);
 
 }
