f164bb94509604c988701159743c4284eab9cd91
Felipe Rodrigues de Almeida <100658381+FelipeAlmeida86@users.noreply.github.com>
2024-08-12 21:37:40 -0300

    Updade: Criação do teste Controller Municipio

--- backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/MunicipioControllerTest.java
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/MunicipioControllerTest.java
@@ -7,10 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import br.com.itilh.bdpedidos.sistemapedidos.exception.IdInexistenteException;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @AutoConfigureMockMvc // modelo, visão e controle
@@ -25,5 +29,42 @@ public class MunicipioControllerTest {
         mockMvc.perform(get("/municipios")).andExpect(status().isOk())
                 .andExpect(content().string(containsString("totalElements")));
 
+    }
+
+    @Test
+    @DisplayName("Teste de path inexistente")
+    void testGetPathInexistente() throws Exception {
+        mockMvc.perform(get("/municipio")).andExpect(status().isMethodNotAllowed());
+
+    }
+
+    @Test
+    @DisplayName("Teste de id inexistente")
+    void testGetIdInexistente() throws Exception {
+        mockMvc.perform(get("/municipio/999999")).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof IdInexistenteException));
+
+    }
+
+    @Test
+    @DisplayName("Teste de id existente")
+    void testGetIdExistente() throws Exception {
+        mockMvc.perform(get("/municipio/1")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Assis Brasil")));
+
+    }
+
+    @Test
+    @DisplayName("Teste de post de novo Municipio")
+    void TesteNovoMunicipio() throws Exception {
+        mockMvc.perform(post("/municipio").contentType("application/json")
+                .content("{\r\n" + //
+                        "  \"id\": 0,\r\n" + //
+                        "  \"nome\": \"teste10\",\r\n" + //
+                        "  \"entrega\": true,\r\n" + //
+                        "  \"estadoId\": 1,\r\n" + //
+                        "  \"estadoNome\": \"acre\"\r\n" + //
+                        "}"))
+                .andExpect(status().isOk());
     }
 }
