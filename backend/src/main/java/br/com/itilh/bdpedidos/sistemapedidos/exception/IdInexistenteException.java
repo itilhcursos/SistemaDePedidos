4692a7ea5428651166ace95c14ee6fbf2ae82bd0
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-08-16 18:37:45 -0300

    feat: add.@ResponseStatus

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/exception/IdInexistenteException.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/exception/IdInexistenteException.java
@@ -2,6 +2,10 @@ package br.com.itilh.bdpedidos.sistemapedidos.exception;
 
 import java.math.BigInteger;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class IdInexistenteException extends RuntimeException {
 
     public IdInexistenteException (String nomeClasse, BigInteger id){
