c6f2808e91f2cfd0a71760daec3f78d8a6c06473
Brinicio Kill Pereira <brinicio13@gmail.com>
2024-08-21 16:53:57 -0300

    Criações test

---
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/FormaPagamentoTest.java
@@ -0,0 +1,13 @@
+package br.com.itilh.bdpedidos.sistemapedidos.controller;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc 
+@ActiveProfiles("test") 
+public class FormaPagamentoTest {
+
+}
---
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/service/ProdutoServiceTest.java
@@ -0,0 +1,10 @@
+package br.com.itilh.bdpedidos.sistemapedidos.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ProdutoServiceTest {
+
+}
