3c0795dc915ff73e64663c1bc2e990085f4c4c19
Brinicio Kill Pereira <brinicio13@gmail.com>
2024-08-23 16:50:00 -0300

    criações do test do service "continuar amanha"

---
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/service/FormaPagamentoServiceTest.java
@@ -0,0 +1,30 @@
+package br.com.itilh.bdpedidos.sistemapedidos.service;
+
+import org.junit.jupiter.api.Test;
+
+public class FormaPagamentoServiceTest {
+    @Test
+    void testAlterarFormaPagamento() {
+
+    }
+
+    @Test
+    void testBuscarFormaPagamentoPorId() {
+
+    }
+
+    @Test
+    void testCriarFormaPagamento() {
+
+    }
+
+    @Test
+    void testExcluirFormaPagamento() {
+
+    }
+
+    @Test
+    void testListarFormasPagamento() {
+
+    }
+}
