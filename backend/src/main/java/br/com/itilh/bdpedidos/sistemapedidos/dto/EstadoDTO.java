cb61b34825de91f309c03598b09613968ab11ef5
itilhcursos <itilhcursos@gmail.com>
2024-08-23 21:31:56 -0300

    Update: refatoração da classe Estado e incio dos teste

---
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/dto/EstadoDTO.java
@@ -0,0 +1,21 @@
+package br.com.itilh.bdpedidos.sistemapedidos.dto;
+
+import java.math.BigInteger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class EstadoDTO {
+    
+    private BigInteger id;
+    private String nome;
+
+}
