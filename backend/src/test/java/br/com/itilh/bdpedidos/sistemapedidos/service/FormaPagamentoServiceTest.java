878cf47ef9a80c0d7fe0f711dd7dc63fb2dcae1f
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-08-25 11:55:56 -0300

    feat: teste_alterar_forma_pagamento_service_test

--- backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/service/FormaPagamentoServiceTest.java
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/service/FormaPagamentoServiceTest.java
@@ -1,5 +1,56 @@
 package br.com.itilh.bdpedidos.sistemapedidos.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.itilh.bdpedidos.sistemapedidos.dto.FormaPagamentoDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.dto.MunicipioDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.exception.FormaPagamentoDuplicadoException;
+import br.com.itilh.bdpedidos.sistemapedidos.model.Estado;
+import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
+import br.com.itilh.bdpedidos.sistemapedidos.repository.FormaPagamentoRepository;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class FormaPagamentoServiceTest {
+
+@Autowired
+FormaPagamentoRepository formaPagamentoRepository;
+
+@Autowired
+FormaPagamentoService formaPagamentoService;
+
+
+void setupFormaPagamento() {
+        FormaPagamento formaPagamento = new FormaPagamento(BigInteger.ONE,"Forma Pagamento",true);
+        formaPagamentoRepository.save(formaPagamento);
+    }
+
+@Test
+    void testCriarFormaPagamento() throws Exception {
+        FormaPagamentoDTO dto = new FormaPagamentoDTO(null,"Forma Pagamento",true);
+        FormaPagamentoDTO dtoRetorno = formaPagamentoService.criarFormaPagamento(dto);
+        assertEquals(true, dtoRetorno.getId() != null);
+    }
+
+ @Test
+    @DisplayName("Teste de regra de negócio -  Forma de Pagamento Duplicada")
+    void testCriarFormaPagamentoDuplicada() throws Exception {
+        setupFormaPagamento();
+        FormaPagamentoDTO dto = new FormaPagamentoDTO(null,"Forma Pagamento duplicada",true);
+        FormaPagamentoDTO dtoRetorno = formaPagamentoService.criarFormaPagamento(dto);
+        
+        assertThrows(FormaPagamentoDuplicadoException.class, ()-> formaPagamentoService.criarFormaPagamento(dto));
+    }
+
 
 }
