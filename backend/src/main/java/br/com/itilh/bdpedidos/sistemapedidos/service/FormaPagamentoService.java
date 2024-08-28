90fdef03cc82d476dede6cb89102ef646c42e031
Gustavo201991 <gustavoruppneto@gmail.com>
2024-08-27 22:08:58 -0300

    commit formapagamento teste

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/service/FormaPagamentoService.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/service/FormaPagamentoService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.itilh.bdpedidos.sistemapedidos.dto.FormaPagamentoDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.exception.FormaPagamentoDuplicadoException;
 import br.com.itilh.bdpedidos.sistemapedidos.exception.IdInexistenteException;
 import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
 import br.com.itilh.bdpedidos.sistemapedidos.repository.FormaPagamentoRepository;
@@ -35,9 +36,19 @@ public class FormaPagamentoService {
         .orElseThrow(()-> new IdInexistenteException("Forma de Pagamento", id)));
     }
 
+    //public FormaPagamentoDTO criarFormaPagamento(FormaPagamentoDTO origem) throws Exception {    
+        //return toDTO(repositorio.save(toEntity(origem)));
+    //}
     public FormaPagamentoDTO criarFormaPagamento(FormaPagamentoDTO origem) throws Exception {    
+    if(repositorio.existsByDescricao(origem.getDescricao()))   
+        throw new FormaPagamentoDuplicadoException(origem.getDescricao());
+
+    try {    
         return toDTO(repositorio.save(toEntity(origem)));
+    } catch (Exception e) {
+        throw new Exception("Erro ao salvar a Forma de Pagamento.", e);
     }
+}
 
     public FormaPagamentoDTO alterarFormaPagamento(BigInteger id, FormaPagamentoDTO origem) throws Exception {
         return toDTO(repositorio.save(toEntity(origem)));
