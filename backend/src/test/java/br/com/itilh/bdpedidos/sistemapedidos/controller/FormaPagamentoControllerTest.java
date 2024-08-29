279f21cfb8e13fa5eb49b2087cac060a6d52d0ce
Guilherme Vilaruel <guilhermevillaruel@gmail.com>
2024-08-28 21:53:25 -0300

    update-controllertest

--- backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/FormaPagamentoControllerTest.java
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/FormaPagamentoControllerTest.java
@@ -1,6 +1,5 @@
 package br.com.itilh.bdpedidos.sistemapedidos.controller;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import br.com.itilh.bdpedidos.sistemapedidos.exception.IdInexistenteException;
 import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
 import br.com.itilh.bdpedidos.sistemapedidos.repository.FormaPagamentoRepository;
-
-
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -34,7 +31,6 @@ public class FormaPagamentoControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-    
 
     @Autowired
     FormaPagamentoRepository FormaPagamentoRepository;
@@ -45,91 +41,81 @@ public class FormaPagamentoControllerTest {
     @DisplayName("Teste do path /formas-pagamento")
     void testGetFormasPagamento() throws Exception {
         mockMvc.perform(get("/formas-pagamento")).andExpect(status().isOk())
-        .andExpect(content().string(containsString("totalElements")));
+                .andExpect(content().string(containsString("totalElements")));
 
     }
 
     @Test
     @DisplayName("teste de path inexistente")
-    void TesteGetPathInexistente() throws Exception{
+    void TesteGetPathInexistente() throws Exception {
         mockMvc.perform(get("/forma-pagamento")).andExpect(status().isMethodNotAllowed());
     }
 
-
     @Test
     @DisplayName("teste de id existente ")
-    void TesteGetIdExistente() throws Exception{
+    void TesteGetIdExistente() throws Exception {
         setupFormaPagamento();
         mockMvc.perform(get("/forma-pagamento/1")).andExpect(status().isOk())
-        .andExpect(content().string(containsString("Forma de Pagamento teste")));
+                .andExpect(content().string(containsString("Forma de Pagamento teste")));
     }
-
 
     @Test
     @DisplayName("teste de id inexitente")
     void TesteGetIdInexistente() throws Exception {
-        mockMvc.perform(get("/formapagamento/9999999")).andExpect(status().isBadRequest())
-        .andExpect(result -> assertTrue(result.getResolvedException() instanceof IdInexistenteException));
+        mockMvc.perform(get("/forma-pagamento/9999999")).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof IdInexistenteException));
     }
 
     @Test
     @DisplayName("teste de path errado")
-    void TesteGetPathErrado() throws Exception{
+    void TesteGetPathErrado() throws Exception {
         mockMvc.perform(get("/forma-pagamentoxptoyzujmkpto")).andExpect(status().isNotFound());
     }
 
-    void setupFormaPagamento(){
+    void setupFormaPagamento() {
         FormaPagamento formaPagamento = new FormaPagamento(
-            BigInteger.ONE,
-            "Forma de Pagamento",
-            true);
-            FormaPagamentoRepository.save(formaPagamento);
-    }
-
-
-    @Test
-    @DisplayName("teste de post de nova Forma de Pagamento")
-    void TestePostFormaPagamento() throws Exception{
-        setupFormaPagamento();
-        mockMvc.perform( 
-            post("/forma-pagamento")
-            .contentType("application/json")
-            .content("{\r\n" + //
-                    "  \"id\": 0,\r\n" + //
-                    "  \"descricao\": \"Forma de Pagamento teste\",\r\n" + //
-                    "  \"ativo\": true,\r\n" + //
-                                "}")        
-        ).andExpect(status().isOk());
+                BigInteger.ONE,
+                "Forma de Pagamento teste",
+                true);
+        FormaPagamentoRepository.save(formaPagamento);
     }
 
     @Test
-    @DisplayName("teste de put de nova Forma de Pagamento")
-    void TestePutFormaPagamento() throws Exception{
+    @DisplayName("Teste de post de nova Forma de Pagamento")
+    void TestePostFormaPagamento() throws Exception {
+        mockMvc.perform(
+                post("/forma-pagamento")
+                        .contentType("application/json")
+                        .content("{\r\n" +
+                                "  \"id\": 0,\r\n" +
+                                "  \"descricao\": \"Forma de Pagamento teste\",\r\n" +
+                                "  \"ativo\": true\r\n" +
+                                "}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Teste de put de nova Forma de Pagamento")
+    void TestePutFormaPagamento() throws Exception {
         setupFormaPagamento();
-        mockMvc.perform( 
-            put("/forma-pagamento/1")
-            .contentType("application/json")
-            .content("{\r\n" + //
-                    "  \"id\": 1,\r\n" + 
-                    "  \"descricao\": \"Forma de Pagamento teste Alterado\",\r\n" + 
-                    "  \"ativo\": true,\r\n" + //
-                                "}")        
-        ).andExpect(status().isOk()
-        ).andExpect(content().string(containsString("FormaPagamento teste Alterado")));
+        mockMvc.perform(
+                put("/forma-pagamento/1")
+                        .contentType("application/json")
+                        .content("{\r\n" +
+                                "  \"id\": 1,\r\n" +
+                                "  \"descricao\": \"Forma de Pagamento teste Alterado\",\r\n" +
+                                "  \"ativo\": true\r\n" +
+                                "}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Forma de Pagamento teste Alterado")));
     }
 
     @Test
     @DisplayName(" Teste do delete")
-    void testeDeleteFormaPagamento() throws Exception{
+    void testeDeleteFormaPagamento() throws Exception {
         setupFormaPagamento();
-        mockMvc.perform( delete("/forma-pagamento/1")
-        ).andExpect(status().isOk()
-        ).andExpect(content().string(containsString("Excluído")));
+        mockMvc.perform(delete("/forma-pagamento/1")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Excluído")));
     }
-
-
-
-
-
 
 }
--- backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/ProdutoControllerTest.java
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/ProdutoControllerTest.java
@@ -1,6 +1,5 @@
 package br.com.itilh.bdpedidos.sistemapedidos.controller;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import br.com.itilh.bdpedidos.sistemapedidos.exception.IdInexistenteException;
 import br.com.itilh.bdpedidos.sistemapedidos.model.Produto;
 import br.com.itilh.bdpedidos.sistemapedidos.repository.ProdutoRepository;
-
-
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -35,7 +32,6 @@ public class ProdutoControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-    
 
     @Autowired
     ProdutoRepository produtoRepository;
@@ -46,42 +42,38 @@ public class ProdutoControllerTest {
     @DisplayName("teste do path / produtos ")
     void testGetProdutos() throws Exception {
         mockMvc.perform(get("/produtos")).andExpect(status().isOk())
-        .andExpect(content().string(containsString("totalElements")));
+                .andExpect(content().string(containsString("totalElements")));
 
     }
 
     @Test
     @DisplayName("teste de path inexistente")
-    void TesteGetPathInexistente() throws Exception{
+    void TesteGetPathInexistente() throws Exception {
         mockMvc.perform(get("/produto")).andExpect(status().isMethodNotAllowed());
     }
 
-    /**
-     * @throws Exception
-     */
     @Test
     @DisplayName("teste de id existente ")
     void TesteGetIdExistente() throws Exception {
+        setupProduto();
         mockMvc.perform(get("/produto/1")).andExpect(status().isOk())
-            .andExpect(content().string(containsString("Produto teste")));
+                .andExpect(content().string(containsString("Produto teste")));
 
-    
     }
 
-
     @Test
-    @DisplayName("teste de id inexitente")
-    void TesteGetIdInexistente() throws Exception{
+    @DisplayName("teste de id inexistente")
+    void TesteGetIdInexistente() throws Exception {
         mockMvc.perform(get("/produto/9999999")).andExpect(status().isBadRequest())
-        .andExpect(result -> assertTrue(result.getResolvedException() instanceof IdInexistenteException));
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof IdInexistenteException));
     }
 
     @Test
     @DisplayName("teste de path errado")
-    void TesteGetPathErrado() throws Exception{
+    void TesteGetPathErrado() throws Exception {
         mockMvc.perform(get("/produtoxptoyzujmkpto")).andExpect(status().isNotFound());
     }
-    
+
     void setupProduto() {
         Produto produto = new Produto(
                 BigInteger.ONE,
@@ -89,62 +81,51 @@ public class ProdutoControllerTest {
                 0.0,
                 BigDecimal.valueOf(0.00),
                 true);
-                produtoRepository.save(produto);
+        produtoRepository.save(produto);
     }
 
-    
     @Test
     @DisplayName("teste de post de novo produto")
-    void TestePostProduto() throws Exception{
-        mockMvc.perform( 
-            post("/produto/1")
-            .contentType("application/json")
-            .content("{\r\n" + //
-                    "  \"id\": 1,\r\n" + //
-                    "  \"descrição\": \"Produto teste\",\r\n" + 
-                    "  \"quantidadeEstoque\":0 \"\",\r\n" + 
-                    "  \"precoUnidadeAtual\": \"0.0\",\r\n" + //
-                    "  \"ativo\": true,\r\n" + //
-                                "}"))       
-        .andExpect(status().isOk());
-    
+    void TestePostProduto() throws Exception {
+        mockMvc.perform(
+                post("/produto")
+                        .contentType("application/json")
+                        .content("{\r\n" + //
+                                "  \"id\": 0,\r\n" + //
+                                "  \"descrição\": \"Produto teste\",\r\n" +
+                                "  \"quantidadeEstoque\": 0,\r\n" +
+                                "  \"precoUnidadeAtual\": 0.0,\r\n" + //
+                                "  \"ativo\": true\r\n" + //
+                                "}"))
+                .andExpect(status().isOk());
+
     }
 
-
-   
     @Test
     @DisplayName("teste de put de novo produto")
-    void TestePutProduto() throws Exception{
+    void TestePutProduto() throws Exception {
         setupProduto();
-        mockMvc.perform( 
-            put("/produto/1")
-            .contentType("application/json")
-            .content("{\r\n" + //
-                    "  \"id\": 1,\r\n" + //
-                    "  \"descrição\": \"Produto teste\",\r\n" + 
-                    "  \"quantidadeEstoque\":0 \"\",\r\n" + 
-                    "  \"precoUnidadeAtual\": \"0.0\",\r\n" + //
-                    "  \"ativo\": true,\r\n" + //
-                                "}"))       
-        .andExpect(status().isOk())
-        .andExpect(content().string(containsString("Produto teste alterado")));
-    
+        mockMvc.perform(
+                put("/produto/1")
+                        .contentType("application/json")
+                        .content("{\r\n" + //
+                                "  \"id\": 1,\r\n" + //
+                                "  \"descricao\": \"Produto teste alterado\",\r\n" +
+                                "  \"quantidadeEstoque\": 0,\r\n" +
+                                "  \"precoUnidadeAtual\": 0.0,\r\n" + //
+                                "  \"ativo\": true\r\n" + //
+                                "}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Produto teste alterado")));
+
     }
-
-
 
     @Test
     @DisplayName(" Teste do delete")
-    void testeDeleteProduto() throws Exception{
+    void testeDeleteProduto() throws Exception {
         setupProduto();
-        mockMvc.perform( delete("/produto/1")
-        ).andExpect(status().isOk()
-        ).andExpect(content().string(containsString("Excluído")));
+        mockMvc.perform(delete("/produto/1")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Excluído")));
     }
-
-
-
-
-
 
 }
