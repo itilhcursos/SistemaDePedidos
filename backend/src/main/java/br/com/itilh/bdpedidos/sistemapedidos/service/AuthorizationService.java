af140df00901c7fdcb9eb478306cdc0039ca23e2
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-09-09 16:15:22 -0300

    feat: AuthorizationService

---
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/service/AuthorizationService.java
@@ -0,0 +1,9 @@
+package br.com.itilh.bdpedidos.sistemapedidos.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+}
