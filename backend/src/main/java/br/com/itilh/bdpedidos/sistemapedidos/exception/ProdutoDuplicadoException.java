f5b4b3aa9b5b388432d9cbcfc6e80daae88d4fc1
itilhcursos <itilhcursos@gmail.com>
2024-09-13 14:28:12 -0300

    observações

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/exception/ProdutoDuplicadoException.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/exception/ProdutoDuplicadoException.java
@@ -2,7 +2,7 @@ package br.com.itilh.bdpedidos.sistemapedidos.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+//(HttpStatus.BAD_REQUEST)
 @ResponseStatus(HttpStatus.CONFLICT)
 public class ProdutoDuplicadoException extends RuntimeException {
 
