cdea1b528cd68a9eab6cbece1103e3e6e217091b
Brinicio Kill Pereira <brinicio13@gmail.com>
2024-09-17 17:11:35 -0300

    finalizaçao do service do item ItemPedido depois da academia faço os restantes

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/service/ItemPedidoService.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/service/ItemPedidoService.java
@@ -1,5 +1,52 @@
 package br.com.itilh.bdpedidos.sistemapedidos.service;
 
-public class ItemPedidoService {
+import java.math.BigInteger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import br.com.itilh.bdpedidos.sistemapedidos.dto.ItemPedidoDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.exception.IdInexistenteException;
+import br.com.itilh.bdpedidos.sistemapedidos.model.ItemPedido;
+import br.com.itilh.bdpedidos.sistemapedidos.repository.ItemPedidoRepository;
+
+public class ItemPedidoService extends GenericService<ItemPedido,ItemPedidoDTO> {
+
+@Autowired
+private ItemPedidoRepository itemPedidoRepository;
+
+public Page<ItemPedidoDTO> listaritemPedidos(Pageable pageable){
+     return toPageDTO(itemPedidoRepository.findAll(pageable));
+}
+
+ public Page<ItemPedidoDTO> listarItemPedidoPorPedidoId(BigInteger id, Pageable pageable) {
+        return toPageDTO(itemPedidoRepository.findBypedidoId(id, pageable));
+    }
+    public Page<ItemPedidoDTO> listarItemPedidoPorProdutoId(BigInteger id, Pageable pageable) {
+        return toPageDTO(itemPedidoRepository.findByprodutoId(id, pageable));
+    }
+
+    public Page<ItemPedidoDTO> listarItemPedidoPorProdutoNome(String nome, Pageable pageable) {
+        return toPageDTO(itemPedidoRepository.findByprodutoNomeIgnoreCase(nome, pageable));
+    }
+     public ItemPedidoDTO buscarItemPEdidoPorId(BigInteger id) throws Exception {
+        return toDTO(itemPedidoRepository.findById(id)
+        .orElseThrow(()-> new IdInexistenteException("ItemPedido", id)));
+    }
+    public ItemPedidoDTO criarMunicipio(ItemPedidoDTO origem) throws Exception {    
+        return toDTO(itemPedidoRepository.save(toEntity(origem)));
+    }
+public ItemPedidoDTO alterarItemPedido (BigInteger id, ItemPedidoDTO origem) throws Exception {
+        return toDTO(itemPedidoRepository.save(toEntity(origem)));
+    }
+
+    public String excluirItemPedido(BigInteger id) throws Exception{
+        try{ 
+            itemPedidoRepository.deleteById(id);
+             return "Excluído com sucesso";
+        }catch (Exception ex){
+            throw new Exception("Não foi possível excluir o id informado." + ex.getMessage());
+        }
+    }
 
 }
