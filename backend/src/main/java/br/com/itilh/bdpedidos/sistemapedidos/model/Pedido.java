695f3cfe71536b91bdbd662fbac8c0bb48504ca5
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-09-19 18:14:16 -0300

    feat: Pedido.java_package_model

---
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/model/Pedido.java
@@ -0,0 +1,53 @@
+package br.com.itilh.bdpedidos.sistemapedidos.model;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "tb_pedidos")
+public class Pedido {
+    @Id
+    @SequenceGenerator(name = "sequencial", sequenceName = "tb_pedidos_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequencial")
+    private BigInteger id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_forma_pagamento")
+    private FormaPagamento formaPagamento;
+
+    @Column(name = "int_numero", unique = true)
+    private BigInteger numero;
+
+    @Column(name = "dt_compra")
+    private Date dataCompra;
+
+    @Column(name = "dt_entrega")
+    private Date dataEntrega;
+
+    @Column(name = "dt_pagamento")
+    private Date dataPagamento;
+}
