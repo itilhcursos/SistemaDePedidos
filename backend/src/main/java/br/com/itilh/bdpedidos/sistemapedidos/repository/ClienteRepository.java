98a3a4930d711b82b129550844f8998b392db57e
willianxzy <willianlittig14@gmail.com>
2024-09-19 21:57:28 -0300

    fix: corrigindo consulta de nomeRazaoSocial e Municipio existente.

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ClienteRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package br.com.itilh.bdpedidos.sistemapedidos.repository;
 
 import java.math.BigInteger;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,8 @@ import br.com.itilh.bdpedidos.sistemapedidos.model.Cliente;
 
 @Repository
 public interface ClienteRepository  extends JpaRepository<Cliente, BigInteger> {
-    boolean existsByNomeRazaoSocial (String nomeRazaoSocial);
+
+    Page<Cliente> findByMunicipioNomeIgnoreCase(String nome, Pageable pageable);
+
+    boolean existsByNomeRazaoSocialAndMunicipioId(String nomeRazaoSocial, BigInteger id);
 }
