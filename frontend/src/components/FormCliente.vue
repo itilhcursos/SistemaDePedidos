ac87e1968938ed0c7258001179c8c7ad63a038ec
Junior Ferri <juniorferri91@gmail.com>
2024-09-20 12:05:31 -0300

    Update formulário de clientes e ListarClientes

---
+++ frontend/src/components/FormCliente.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="container">
+      <h4 class="p-1 mb-1 bg-success text-white">{{ getAcao }} Cliente</h4>
+      <hr />
+      <form>
+        <div class="mb-3">
+          <label class="form-label">Id</label>
+          <input
+            class="form-control"
+            type="text"
+            v-model="id"
+            :disabled="true"
+            placeholder="Id cliente"
+          />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">NOME</label>
+          <input
+            class="form-control"
+            type="text"
+            v-model="nomeRazaoSocial"
+            placeholder="Nome"
+          />
+        </div>
+        <div v-if="isInvalido" class="alert alert-danger d-flex align-items-center" role="alert">
+          <i class="bi bi-exclamation-triangle-fill"></i>
+          <div class="p-2">{{ mensagem }}</div>
+        </div>
+        <div class="mb-3 d-flex justify-content-end">
+          <button
+            class="btn btn-primary m-2"
+            type="submit"
+            v-on:click.prevent="salvarCliente"
+          >
+          <i class="bi bi-clipboard2-check"></i>
+            {{ getAcao }}
+          </button>
+          <button
+            class="btn btn-warning m-2"
+            type="submit"
+            v-on:click.prevent="cancelar"
+          >
+          <i class="bi bi-clipboard2-x"></i>
+            Cancelar
+          </button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  export default {
+    props: {
+      propsCliente: Object,
+    },
+    data() {
+      return {
+        id: "",
+        nomeRazaoSocial: "",
+        isInvalido: false,
+        mensagem : '',
+      };
+    },
+    methods: {
+      async salvarCliente() {
+        if (this.nome === "") {
+          this.isInvalido = true;
+          this.mensagem = "Nome deve ser preenchido!!";
+          return;
+        }
+        this.isInvalido = false;
+        let config = {
+          headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          }
+        }
+  
+      try{
+          if (this.id === "") {
+            //incluir pelo POST da API
+            const response = await axios.post("http://localhost:8080/cliente", {
+              id: this.id,
+              nome: this.nome,
+            }, config);
+            this.listaClientes = response.data;
+          } else {
+            // alterar pelo PUT da API
+            const response = await axios.put(
+              `http://localhost:8080/cliente/${this.id}`,
+              {
+                id: this.id,
+                nome: this.nome,
+              }
+            ,config );
+            this.listaCliente = response.data;
+          }
+          this.$emit("salvar_cliente", {
+          id: this.id,
+          nome: this.nome,
+        });
+  
+        this.id = "";
+        this.nome = "";
+      }catch(error){
+        //mesagens de erro
+         //exibe o objeto do error completo
+          // console.log (error);
+         //exibe o codigo do status de retorno       
+         // console.log (error.response.status);
+          //exibe o mensagem de erro personalidado do backend
+          // console.log (error.response.data.exception);
+        this.isInvalido = true;
+        if(error.response.status === 403){        
+          this.mensagem = "Usuário não identificado! Faça o login!!!";
+        }else if(error.response.status === 400 &&
+                 error.response.data.exception === 'ClienteDuplicadoException'){
+          this.mensagem = error.response.data.mensagem;     
+        }else{
+          this.mensagem = error.message;
+        }
+      }
+     },
+      cancelar() {
+        this.id = "";
+        this.nome = "";
+        this.$emit("cancelar", true);
+      },
+    },
+    mounted() {
+      if (this.propsCliente) {
+        this.id = this.propsEstado.id;
+        this.nome = this.propsEstado.nome;
+      }
+    },
+    computed: {
+      getAcao() {
+        return this.id === "" ? "Incluir" : "Alterar";
+      },
+    },
+  };
+  </script>
+  
+  
---
+++ frontend/src/components/icons/ListarCliente.vue
@@ -0,0 +1,196 @@
+<template>
+    <div class="container">
+      <div class="row">
+        <div class="col-10">
+          <h3>CLIENTES</h3>
+        </div>
+        <div class="col-2 d-flex justify-content-end">
+          <button v-if="!formVisible" @click="novoCliente" class="btn btn-success">
+            <i class="bi bi-clipboard-plus"></i> Novo
+          </button>
+        </div>
+        <div class="row">
+          <div>
+            <FormCliente
+              v-if="formVisible"
+              :propsCliente="clienteEscolhido"
+              @cancelar="limpar"
+              @salvar_cliente="buscarClientes"
+            />
+          </div>
+        </div>
+      </div>
+  
+      <table class="table table-dark table-striped" v-if="!formVisible">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col" class="d-flex justify-content-end">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="cliente in listaClientes" :key="cliente.id" scope="row">
+            <th>
+              {{ cliente.id }}
+            </th>
+            <td>
+              {{ cliente.nomeRazaoSocial }}
+            </td>
+            <td class="d-flex justify-content-end">
+              <button
+                class="btn btn-btn btn-primary m-2"
+                @click="alterarCliente(cliente)"
+              >
+                <i class="bi bi-clipboard-pulse"></i> Alterar
+              </button>
+  
+              <button
+                class="btn btn-outline-danger m-2"
+                @click="excluirCliente(cliente.id)"
+              >
+                <i class="bi bi-clipboard2-minus"></i> Excluir
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-if="!formVisible">
+      <hr />
+      <div class="container">
+        <div class="row d-flex justify-content-center">
+          <div class="col-auto">
+  
+            <button
+              v-for="pagina in totalPages"
+              :key="pagina"
+              @click.prevent="irPara(pagina)"
+              class="btn btn-light ms-1"
+            >
+              {{ pagina }}
+            </button>
+  
+  
+          </div>
+          <div class="col-auto">
+            <input
+              type="text"
+              v-model="pageNumber"
+              placeholder="Número da pagina"
+              class="form-control w-25"
+            />
+          </div>
+          <div class="col-auto">
+            <select v-model="pageSize" class="form-select">
+              <option value="2">2</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+            </select>
+          </div>
+          <div class="col-auto">
+            <select v-model="property" class="form-select">
+              <option value="id">ID</option>
+              <option value="nome">Nome</option>
+            </select>
+          </div>
+          <div class="col-auto">
+            <select v-model="direction" class="form-select">
+              <option value="ASC">Crescente</option>
+              <option value="DESC">Decrescente</option>
+            </select>
+          </div>
+          <div class="col-auto">
+            <button @click.prevent="buscarClientes" class="btn btn-success">
+              <i class="bi bi-binoculars"></i>
+              Buscar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
+  import FormCliente from "./FormCliente.vue";
+  import axios from "axios";
+  export default {
+    components: {
+      FormCliente,
+    },
+    data() {
+      return {
+        listaClientes: [],
+        clienteEscolhido: null,
+        formVisible: false,
+        mode: import.meta.env.MODE,
+        url: import.meta.env.VITE_APP_URL_API,
+        pageNumber: 1,
+        pageSize: 10,
+        direction: "ASC",
+        property: "id",
+        totalPages: 0,
+      };
+    },
+    methods: {
+      async buscarClientes() {
+        this.clienteEscolhido = null;
+        this.formVisible = false;
+        //buscar a lista de estados no servidor
+        // http://localhost:8080/estados
+        const response = await axios.get(
+          `http://localhost:8080/clientes?pageNumber=${this.pageNumber}&pageSize=${this.pageSize}&direction=${this.direction}&property=${this.property}`
+        );
+        console.log(response.data);
+        this.listaClientes = response.data.content;
+        this.totalPages = response.data.totalPages;
+        console.log(this.totalPages);
+      },
+      limpar() {
+        this.clienteEscolhido = null;
+        this.formVisible = !this.formVisible;
+      },
+      novoCliente() {
+        this.formVisible = !this.formVisible;
+      },
+      alterarCliente(cliente) {
+        this.clienteEscolhido = cliente;
+        this.formVisible = true;
+      },
+      async excluirCliente(id) {
+        // if(localStorage.getItem('token') === null) {
+        //     alert("Usuário não identificado! Faça o login!!!");
+        //     return;
+        // }
+        let config = {
+          headers: {
+            'Authorization': 'Bearer ' +localStorage.getItem('token')
+          }
+        }
+        try{
+            const response = await axios.delete(`http://localhost:8080/cliente/${id}`, config);
+            console.log(response.data);
+        }catch(error){
+          if(error.response.status === 403){        
+           alert("Usuário não identificado! Faça o login!!!");
+          }else if(error.response.status === 400 ){
+            alert(error.response.data.mensagem);     
+          }else{
+            alert(error.message);
+          }
+        }     
+        this.buscarClientes();
+      },
+      irPara(pagina) {
+        this.pageNumber = pagina;
+        this.buscarClientes();
+      },
+    },
+    mounted() {
+      this.buscarClientes();
+    },
+  };
+  </script>
+  
