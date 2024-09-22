95d291e59b7bb4b1d8ca277fa8585f7a60a9123a
Guilherme Vilaruel <guilhermevillaruel@gmail.com>
2024-09-22 09:02:31 -0300

    update_dto

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/dto/ClienteDTO.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/dto/ClienteDTO.java
@@ -1,5 +1,22 @@
 package br.com.itilh.bdpedidos.sistemapedidos.dto;
 
+import java.math.BigInteger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClienteDTO {
-    
+
+    private BigInteger id;
+    private String nome;
+    private Boolean ativo;
+
+    private BigInteger municipioId;
+    private String estadoNome;
+
+
 }
--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/dto/ItemPedidoDTO.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/dto/ItemPedidoDTO.java
@@ -1,5 +1,19 @@
 package br.com.itilh.bdpedidos.sistemapedidos.dto;
 
+import java.math.BigInteger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemPedidoDTO {
-    
+
+
+    private BigInteger id;
+    private String descricao;
+    private Double quantidadeEstoque;
+    private Boolean ativo;
 }
