2752f1dc8ff825f32837fc5840d9b40069f46a94
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-09-30 19:26:38 -0300

    npm install vue-select@beta

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vSelect from "vue-select";
 
 const app = createApp(App)
-
+app.component('v-select', vSelect)
 app.use(router)
 
 app.mount('#app')
