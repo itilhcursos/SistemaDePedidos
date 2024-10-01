3e8fc5e79c31eeab9a732d1b7f7e93de1504b4a8
WillianKuhnLittig <willianlittig14@gmail.com>
2024-10-01 17:19:42 -0300

    fix: método busca produtoService.

--- frontend/src/services/produtoService.js
+++ frontend/src/services/produtoService.js
@@ -3,11 +3,9 @@ import genericService from "./genericService";
 //const path ='/produto';
 const pathGet ='/produtos';
 
-const buscar = async (txtBusca, pageNumber = 1, pageSize = 10000000, direction = 'ASC', property ='id') =>{
-
-    const {data} = await genericService.buscar(pathGet, pageNumber, pageSize, direction, property, txtBusca);
+const buscar = async(txtBusca, pageNumber = 1, pageSize = 100000, direction = 'ASC' , property = 'id') => {
+    const {data} = await genericService.buscar(pathGet, txtBusca, pageNumber, pageSize, direction, property);
     return data;
-
 }
 
 export default{
