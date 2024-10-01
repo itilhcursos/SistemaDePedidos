733f3f9f5b66facfd52c06d9ad93cbea71708e4d
WillianKuhnLittig <willianlittig14@gmail.com>
2024-10-01 11:46:10 -0300

    refactor: funções

--- frontend/src/components/FormEstado.vue
+++ frontend/src/components/FormEstado.vue
@@ -5,42 +5,23 @@
     <form>
       <div class="mb-3">
         <label class="form-label">Id</label>
-        <input
-          class="form-control"
-          type="text"
-          v-model="id"
-          :disabled="true"
-          placeholder="Id estado"
-        />
+        <input class="form-control" type="text" v-model="id" :disabled="true" placeholder="Id estado" />
       </div>
       <div class="mb-3">
         <label class="form-label">Nome</label>
-        <input
-          class="form-control"
-          type="text"
-          v-model="nome"
-          placeholder="Nome"
-        />
+        <input class="form-control" type="text" v-model="nome" placeholder="Nome" />
       </div>
       <div v-if="isInvalido" class="alert alert-danger d-flex align-items-center" role="alert">
         <i class="bi bi-exclamation-triangle-fill"></i>
         <div class="p-2">{{ mensagem }}</div>
       </div>
       <div class="mb-3 d-flex justify-content-end">
-        <button
-          class="btn btn-primary m-2"
-          type="submit"
-          v-on:click.prevent="salvarEstado"
-        >
-        <i class="bi bi-clipboard2-check"></i>
+        <button class="btn btn-primary m-2" type="submit" v-on:click.prevent="salvarEstado">
+          <i class="bi bi-clipboard2-check"></i>
           {{ getAcao }}
         </button>
-        <button
-          class="btn btn-warning m-2"
-          type="submit"
-          v-on:click.prevent="cancelar"
-        >
-        <i class="bi bi-clipboard2-x"></i>
+        <button class="btn btn-warning m-2" type="submit" v-on:click.prevent="cancelar">
+          <i class="bi bi-clipboard2-x"></i>
           Cancelar
         </button>
       </div>
@@ -62,54 +43,58 @@ export default {
     };
   },
   methods: {
-    getDados(){
+    getDados() {
       return {
-          id: this.id,
-          nome: this.nome,
-        }
-      },
+        id: this.id,
+        nome: this.nome,
+      }
+    },
     async salvarEstado() {
-      if (this.nome === "") {
+      if (!this.nome) {
         this.isInvalido = true;
         this.mensagem = "Nome Deve ser Preenchido";
         return;
       }
       this.isInvalido = false;
 
-      try{
-          if (this.id === "") {
-            const response = await estadoService.criar(this.getDados());
-            this.listaEstados = response;
-          } else {
-            const response = await estadoService.atualizar(
-              this.id,
-              this.getDados());
-            this.listaEstados = response;
-          }
-          this.$emit("salvar_estado", {
-          id: this.id,
-          nome: this.nome,
-        });
+      try {
+        if (!this.id) {
+          const response = await estadoService.criar(this.getDados());
+          this.listaEstados = response;
+        } else {
+          const response = await estadoService.atualizar(
+            this.id,
+            this.getDados());
+          this.listaEstados = response;
+        }
+        this.$emit("salvar_estado", this.getDados());
 
-        this.id = "";
-        this.nome = "";
-      }catch(error){
-        this.isInvalido = true;
-        if(error.response.status === 403){        
-          this.mensagem = "Usuário não identificado! Faça o login!!!";
-        }else if(error.response.status === 400 &&
-                error.response.data.exception === 'EstadoDuplicadoException'){
-        this.mensagem = error.response.data.mensagem;     
-      }else{
+        this.limparFormulario();
+      } catch (error) {
+        this.tratarErro(error);
+      };
+    },
+    cancelar() {
+      this.limparFormulario();
+      this.$emit("cancelar", true);
+    },
+    limparFormulario() {
+      this.id = "";
+      this.nome = "";
+    },
+    tratarErro(error) {
+      this.isInvalido = true;
+      if (error.response.status === 403) {
+        this.mensagem = "Usuário não identificado! Faça o login!!!";
+      } else if (
+        error.response.status === 400 &&
+        error.response.data.exception === "EstadoDuplicadoException"
+      ) {
+        this.mensagem = error.response.data.mensagem;
+      } else {
         this.mensagem = error.message;
       }
     }
-    },
-    cancelar() {
-      this.id = "";
-      this.nome = "";
-      this.$emit("cancelar", true);
-    },
   },
   mounted() {
     if (this.propsEstado) {
@@ -124,4 +109,3 @@ export default {
   },
 };
 </script>
-
