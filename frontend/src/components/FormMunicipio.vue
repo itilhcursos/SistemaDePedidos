1d4f7826e650b2f24a1de4fb707817134beb6c55
WillianKuhnLittig <willianlittig14@gmail.com>
2024-10-01 10:35:29 -0300

    fix: correção alteração de municipios + refatoração de algumas funções.

--- frontend/src/components/FormMunicipio.vue
+++ frontend/src/components/FormMunicipio.vue
@@ -5,22 +5,11 @@
     <form>
       <div class="mb-3">
         <label class="form-label">ID</label>
-        <input
-          class="form-control"
-          type="text"
-          v-model="id"
-          :disabled="true"
-          placeholder="Id Municipio"
-        />
+        <input class="form-control" type="text" v-model="id" :disabled="true" placeholder="Id Municipio" />
       </div>
       <div class="mb-3">
         <label class="form-label">Nome</label>
-        <input
-          class="form-control"
-          type="text"
-          v-model="nome"
-          placeholder="Nome"
-        />
+        <input class="form-control" type="text" v-model="nome" placeholder="Nome" />
       </div>
       <div class="mb-3">
         <label class="form-label">Entrega</label>
@@ -30,14 +19,8 @@
         </select>
       </div>
       <label class="form-label">Estado</label>
-      <v-select
-        class="form-control"
-        label="Estado"
-        :filterable="false"
-        v-model="estadoSelecionado"
-        :options="estados"
-        @search="onSearchEstados"
-      >
+      <v-select class="form-control" label="Estado" :filterable="false" v-model="estadoSelecionado" :options="estados"
+        @search="onSearchEstados">
         <template v-slot:no-options> Sem Estados para exibir. </template>
         <template v-slot:option="option">
           {{ option.nome }}
@@ -46,28 +29,16 @@
           {{ option.nome }}
         </template>
       </v-select>
-      <div
-        v-if="isInvalido"
-        class="alert alert-danger d-flex align-items-center"
-        role="alert"
-      >
+      <div v-if="isInvalido" class="alert alert-danger d-flex align-items-center" role="alert">
         <i class="bi bi-exclamation-triangle-fill"></i>
         <div class="p-2">{{ mensagem }}</div>
       </div>
       <div class="mb-3 d-flex justify-content-end">
-        <button
-          class="btn btn-primary m-2"
-          type="submit"
-          v-on:click.prevent="salvarMunicipio"
-        >
+        <button class="btn btn-primary m-2" type="submit" v-on:click.prevent="salvarMunicipio">
           <i class="bi bi-clipboard2-check"></i>
           {{ getAcao }}
         </button>
-        <button
-          class="btn btn-warning m-2"
-          type="submit"
-          v-on:click.prevent="cancelar"
-        >
+        <button class="btn btn-warning m-2" type="submit" v-on:click.prevent="cancelar">
           <i class="bi bi-clipboard2-x"></i>
           Cancelar
         </button>
@@ -109,7 +80,7 @@ export default {
         id: this.id,
         nome: this.nome,
         entrega: this.entrega,
-        estadoSelecionado: this.estadoSelecionado,
+        estadoId: this.estadoSelecionado.id,
       };
     },
     async salvarMunicipio() {
@@ -131,30 +102,17 @@ export default {
           );
           this.listaMunicipios = response;
         }
-        this.$emit("salvar_municipio", {
-          id: this.id,
-          nome: this.nome,
-          entrega: this.entrega,
-          estadoSelecionado: this.estadoId,
-        });
-
-        this.id = "";
-        this.nome = "";
-        this.entrega = "";
-        this.estadoSelecionado = "";
+        this.$emit("salvar_municipio", this.getDados());
+        this.limparFormulario();
       } catch (error) {
-        this.isInvalido = true;
-        if (error.response.status === 403) {
-          this.mensagem = "Usuário não identificado! Faça o login!!!";
-        } else if (
-          error.response.status === 400 &&
-          error.response.data.exception === "MunicipioDuplicadoException"
-        ) {
-          this.mensagem = error.response.data.mensagem;
-        } else {
-          this.mensagem = error.message;
-        }
+        this.tratarErro(error);
       }
+    },
+    limparFormulario() {
+      this.id = "";
+      this.nome = "";
+      this.entrega = "";
+      this.estadoSelecionado = "";
     },
     cancelar() {
       this.id = "";
@@ -163,6 +121,19 @@ export default {
       this.estadoSelecionado = "";
       this.$emit("cancelar", true);
     },
+    tratarErro(error) {
+      this.isInvalido = true;
+      if (error.response.status === 403) {
+        this.mensagem = "Usuário não identificado! Faça o login!!!";
+      } else if (
+        error.response.status === 400 &&
+        error.response.data.exception === "MunicipioDuplicadoException"
+      ) {
+        this.mensagem = error.response.data.mensagem;
+      } else {
+        this.mensagem = error.message;
+      }
+    }
   },
   mounted() {
     if (this.propsMunicipio) {
@@ -179,4 +150,3 @@ export default {
   },
 };
 </script>
-
