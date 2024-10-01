150fd42ca2804acc9c89c74873c33cd4e40ca18c
WillianKuhnLittig <willianlittig14@gmail.com>
2024-10-01 17:18:13 -0300

    feat: método busca para forma-pagamento.

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/FormaPagamentoController.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/FormaPagamentoController.java
@@ -39,6 +39,19 @@ public class FormaPagamentoController {
         return formaPagamentoService.listarFormasPagamento(pageable);
     }
 
+    @GetMapping("/formas-pagamento/{txtBusca}")
+    public Page<FormaPagamentoDTO> getPorNome(
+        @RequestParam(required = false, defaultValue = "1") int pageNumber,
+        @RequestParam(required = false, defaultValue = "10") int pageSize,
+        @RequestParam(required = false, defaultValue = "ASC") String direction,
+        @RequestParam(required = false, defaultValue = "id") String property,
+        @PathVariable String txtBusca
+    ) {
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, Sort.Direction.valueOf(direction), property);
+
+        return formaPagamentoService.buscar(pageable, txtBusca);
+    }
+
     // @GetMapping("/formas-pagamento/descricao/{descricao}")
     // public List<FormaPagamentoDTO> getFormaPagamentoPorDescricao(@PathVariable String descricao,
     // @RequestParam(required = true) ModoBusca modoBusca) {
--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
@@ -3,6 +3,8 @@ package br.com.itilh.bdpedidos.sistemapedidos.repository;
 import java.math.BigInteger;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +16,7 @@ public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, 
     List<FormaPagamento> findByDescricao(String descricao);
 
     List<FormaPagamento> findByDescricaoStartingWithIgnoreCase(String descricao);
+    Page<FormaPagamento> findByDescricaoContainingIgnoreCase(Pageable pageable, String descricao);
 
     boolean existsByDescricao (String descricao);
 
