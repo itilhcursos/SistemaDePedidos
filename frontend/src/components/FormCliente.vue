d6c6fffc7d70b668e8959de4cd90503580cb1acf
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-10-03 14:20:21 -0300

    feat: implementação_FormCliente

--- frontend/src/components/FormCliente.vue
+++ frontend/src/components/FormCliente.vue
@@ -3,67 +3,80 @@
         <h4 class="p-1 mb-1 bg-success text-white">{{ getAcao }} Cliente</h4>
         <hr />
         <form>
-            <div class="container">
-                <div class="row">
-                    <div class="col-mb-3">
-                        <label class="form-label">Id</label>
-                        <input class="form-control" type="text" v-model="id" :disabled="true"
-                            placeholder="Id cliente" />
-                    </div>
-                    <div class="col-mb-3">
-                        <label class="form-label">Cliente</label>
-                        <input class="form-control" type="text" v-model="nomeRazaoSocial"
-                            placeholder="Nome ou Razão Social" />
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <label class="form-label">CNPJ</label>
-                        <input class="form-control" type="text" v-model="cnpj" placeholder="CNPJ" />
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label class="form-label">CPF</label>
-                        <input class="form-control" type="text" v-model="cpf" placeholder="CPF" />
-                    </div>
-                    <div class="col-mb-3">
-                        <label class="form-label">Endereço</label>
-                        <input class="form-control" type="text" v-model="endereço"
-                            placeholder="Endereço (Rua, Avenida, etc)" />
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <label class="form-label">Bairro</label>
-                        <input class="form-control" type="text" v-model="bairro" placeholder="Bairro" />
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <label class="form-label">CEP</label>
-                        <input class="form-control" type="text" v-model="cep" placeholder="CEP" />
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <label class="form-label">Telefone</label>
-                        <input class="form-control" type="text" v-model="telefone" placeholder="Telefone (com DDD)" />
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label class="form-label">Email</label>
-                        <input class="form-control" type="text" v-model="email" placeholder="nome@exemplo.com" />
-                    </div>
-                    <div class="col">
-                        <label class="form-label">Informações</label>
-                        <input class="form-control" type="text" v-model="informacoes"
-                            placeholder="Informações complementares" />
-                    </div>
-                </div>
+            <!-- <div class="mb-3">
+                <label class="form-label">Id</label>
+                <input class="form-control" type="text" v-model="id" :disabled="true" placeholder="Id cliente" />
+            </div> -->
+            <div class="mb-3">
+                <label class="form-label">Cliente</label>
+                <input class="form-control" type="text" v-model="nomeRazaoSocial" placeholder="Nome ou Razão Social" />
             </div>
-            <div class="col-mb-3">
-                <label class="form-label">Ativo</label>
-                <select v-model="ativo" class="form-select">
-                    <option :value="true">Sim</option>
-                    <option :value="false">Não</option>
-                </select>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">CNPJ</label>
+                    <input class="form-control" type="text" v-model="cnpj" placeholder="CNPJ" />
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">CPF</label>
+                    <input class="form-control" type="text" v-model="cpf" placeholder="CPF" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Endereço</label>
+                    <input class="form-control" type="text" v-model="endereço"
+                        placeholder="Endereço (Rua, Avenida, Alameda, etc)" />
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Bairro</label>
+                    <input class="form-control" type="text" v-model="bairro" placeholder="Bairro" />
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">CEP</label>
+                    <input class="form-control" type="text" v-model="cep" placeholder="CEP" />
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Telefone</label>
+                    <input class="form-control" type="text" v-model="telefone" placeholder="Telefone (com DDD)" />
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Email</label>
+                    <input class="form-control" type="text" v-model="email" placeholder="nome@exemplo.com" />
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Estado</label>
+                    <select v-model="estadoSelected" class="form-select">
+                        <option v-for="estado in estados" :key="estado.id" :value="estado.id">
+                            {{ estado.nome }}
+                        </option>
+                    </select>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Município</label>
+                    <select v-model="municipioSelected" class="form-select">
+                        <option v-for="municipio in municipios" :key="municipio.id" :value="municipio.id">
+                            {{ municipio.nome }}
+                        </option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Informações</label>
+                    <input class="form-control" type="text" v-model="informacoes"
+                    placeholder="Informações Adicionais" />
+                </div>
+                
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Ativo</label>
+                    <select v-model="ativo" class="form-select">
+                        <option :value="true">Sim</option>
+                        <option :value="false">Não</option>
+                    </select>
+                </div>
             </div>
             <div v-if="isInvalido" class="alert alert-danger d-flex align-items-center" role="alert">
                 <i class="bi bi-exclamation-triangle-fill"></i>
                 <div class="p-2">{{ mensagem }}</div>
             </div>
             <div class="mb-3 d-flex justify-content-end">
-                <button class="btn btn-primary m-2" type="submit" v-on:click.prevent="salvarProduto">
+                <button class="btn btn-primary m-2" type="submit" v-on:click.prevent="salvarCliente">
                     <i class="bi bi-clipboard2-check"></i>
                     {{ getAcao }}
                 </button>
@@ -78,17 +91,28 @@
 
 <script>
 import clienteService from '@/services/clienteService';
+import municipioService from '@/services/municipioService';
+import estadoService from '@/services/estadoService';
 export default {
     props: {
-        propsProduto: Object,
+        propsCliente: Object,
     },
     data() {
         return {
             id: "",
-            descricao: "",
-            urlImagem: "",
-            quantidadeEstoque: "",
-            precoUnidadeAtual: "",
+            nome: "",
+            cnpj: "",
+            cpf: "",
+            endereço: "",
+            bairro: "",
+            cep: "",
+            telefone: "",
+            email: "",
+            informacoes: "",
+            estadoSelected: null,
+            estados: [],
+            municipioSelected: null,
+            municipios: [],
             ativo: "",
             isInvalido: false,
             mensagem: "",
@@ -98,29 +122,36 @@ export default {
         getDados() {
             return {
                 id: this.id,
-                descricao: this.descricao,
-                urlImagem: this.urlImagem,
-                quantidadeEstoque: this.quantidadeEstoque,
-                precoUnidadeAtual: this.precoUnidadeAtual,
+                nome: this.nome,
+                cnpj: this.cnpj,
+                cpf: this.cpf,
+                endereço: this.endereco,
+                bairro: this.bairro,
+                cep: this.cep,
+                telefone: this.cep,
+                email: this.email,
+                informacoes: this.informacoes,
+                estadoId: this.estadoSelected,
+                municipioId: this.municipioSelected,
                 ativo: this.ativo,
             };
         },
-        async salvarProduto() {
-            if (this.descricao === "") {
+        async salvarCliente() {
+            if (this.nome === "" || this.municipioSelected === null || this.estadoSelected === null) {
                 this.isInvalido = true;
-                this.mensagem = "Descrição do produto não pode ser vazia.";
+                this.mensagem = "Nome do cliente ou razão social, município e estado não pode ser vazio.";
                 return;
             }
             this.isInvalido = false;
             try {
                 if (this.id === "") {
                     const response = await clienteService.criar(this.getDados());
-                    this.listaProdutos = response;
+                    this.listaClientes = response;
                 } else {
                     const response = await clienteService.atualizar(this.id, this.getDados());
-                    this.listaProdutos = response;
+                    this.listaClientes = response;
                 }
-                this.$emit("salvar_produto", this.getDados());
+                this.$emit("salvar_cliente", this.getDados());
 
                 // Resetar campos
                 this.resetarCampos();
@@ -133,12 +164,35 @@ export default {
             this.resetarCampos();
             this.$emit("cancelar", true);
         },
+        async carregarEstados() {
+            try {
+                const response = await estadoService.listar(1, 100, 'ASC', 'id');
+                this.estados = response.content;
+            } catch (error) {
+                console.error("Erro ao carregar estados:", error);
+            }
+        },
+        async carregarMunicipios() {
+            try {
+                const response = await municipioService.listar(1, 10000, 'ASC', 'id');
+                this.municipios = response.content;
+            } catch (error) {
+                console.error("Erro ao carregar municipios:", error);
+            }
+        },
         resetarCampos() {
             this.id = "";
-            this.descricao = "";
-            this.urlImagem = "";
-            this.quantidadeEstoque = "";
-            this.precoUnidadeAtual = "";
+            this.nome = "";
+            this.cnpj = "";
+            this.cpf = "";
+            this.endereço = "";
+            this.bairro = "";
+            this.cep = "";
+            this.telefone = "";
+            this.email = "";
+            this.informacoes = "";
+            this.estadoSelected = null;
+            this.municipioSelected = null;
             this.ativo = "";
         },
         getMensagemErro(error) {
@@ -152,14 +206,23 @@ export default {
         }
     },
     mounted() {
-        if (this.propsProduto) {
-            this.id = this.propsProduto.id;
-            this.descricao = this.propsProduto.descricao;
-            this.urlImagem = this.propsProduto.urlImagem;
-            this.quantidadeEstoque = this.propsProduto.quantidadeEstoque;
-            this.precoUnidadeAtual = this.propsProduto.precoUnidadeAtual;
-            this.ativo = this.propsProduto.ativo;
+        if (this.propsCliente) {
+            this.id = this.propsCliente.id;
+            this.nome = this.propsCliente.nome;
+            this.cnpj = this.propsCliente.cnpj;
+            this.cpf = this.propsCliente.cpf;
+            this.endereço = this.propsCliente.endereco;
+            this.bairro = this.propsCliente.bairro;
+            this.cep = this.propsCliente.cep;
+            this.telefone = this.propsCliente.telefone;
+            this.email = this.propsCliente.email;
+            this.informacoes = this.propsCliente.informacoes;
+            this.municipioSelectd = this.propsCliente.municipioId; 
+            this.estadoSelectd = this.propsCliente.estadoId; 
+            this.ativo = this.propsCliente.ativo;
         }
+        this.carregarEstados();
+        this.carregarMunicipios();
     },
     computed: {
         getAcao() {
