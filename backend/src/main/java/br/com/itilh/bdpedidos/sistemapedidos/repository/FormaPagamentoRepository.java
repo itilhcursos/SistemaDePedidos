aba81e76844076ec349390d6b99af64dcb821915
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-10-04 18:07:24 -0300

    feat: FormaPagamento_findByDescricaoContainingIgnoreCase

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/FormaPagamentoRepository.java
@@ -3,10 +3,13 @@ package br.com.itilh.bdpedidos.sistemapedidos.repository;
 import java.math.BigInteger;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.itilh.bdpedidos.sistemapedidos.model.FormaPagamento;
+import br.com.itilh.bdpedidos.sistemapedidos.model.Produto;
 
 @Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, BigInteger> {
@@ -14,6 +17,11 @@ public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, 
     List<FormaPagamento> findByDescricao(String descricao);
 
     List<FormaPagamento> findByDescricaoStartingWithIgnoreCase(String descricao);
+
+    List<FormaPagamento> findByDescricaoContainingIgnoreCase(String descricao);
+
+    Page<FormaPagamento> findByDescricaoContainingIgnoreCase(Pageable pageable, String descricao);
+
 
     boolean existsByDescricao (String descricao);
 
