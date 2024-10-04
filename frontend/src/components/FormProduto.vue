ebe6a6c653821593cef71e0d4c5a5f26fb2c513c
Junior Ferri <juniorferri91@gmail.com>
2024-10-04 20:38:40 -0300

    correção formulário produtos na aula

--- frontend/src/components/FormProduto.vue
+++ frontend/src/components/FormProduto.vue
@@ -5,79 +5,46 @@
         <form>
             <div class="mb-3">
                 <label class="form-label">Id</label>
-                <input
-                class="form-control"
-                type="text"
-                v-model="id"
-                :disabled="true"
-                placeholder="Id produto"
-                />
+                <input class="form-control" type="text" v-model="id" :disabled="true" placeholder="Id produto" />
             </div>
             <div class="mb-3">
                 <label class="form-label">Descrição</label>
-                <input
-                class="form-control"
-                type="text"
-                v-model="descricao"
-                placeholder="Descrição"
-                />
+                <input class="form-control" type="text" v-model="descricao" placeholder="Descrição" />
             </div>
             <div class="mb-3">
                 <label class="form-label">Quantidade em Estoque</label>
-                <input
-                class="form-control"
-                type="text"
-                v-model="quantidadeEstoque"
-                placeholder="Quantidade em Estoque"
-                />
+                <input class="form-control" type="number" v-model="quantidadeEstoque"
+                    placeholder="Quantidade em Estoque" />
             </div>
             <div class="mb-3">
                 <label class="form-label">Preço Unitário</label>
-                <input
-                class="form-control"
-                type="text"
-                v-model="precoUnidadeAtual"
-                placeholder="Preço Unitário"
-                />
+                <input class="form-control" type="number" v-model="precoUnidadeAtual" placeholder="Preço Unitário" />
             </div>
             <div class="mb-3">
                 <label class="form-label">Ativo</label>
                 <select v-model="ativo" class="form-select">
-            <option :value="true">Sim</option>
-            <option :value="false">Não</option>
-          </select>
+                    <option :value="true">Sim</option>
+                    <option :value="false">Não</option>
+                </select>
             </div>
 
             <div class="mb-3">
                 <label class="form-label">URL Imagem</label>
-                <input
-                class="form-control"
-                type="text"
-                v-model="urlImagem"
-                placeholder="URL Imagem"
-                />
+                <input class="form-control" type="text" v-model="urlImagem" placeholder="URL Imagem" />
             </div>
 
             <div v-if="isInvalido" class="alert alert-danger d-flex align-items-center" role="alert">
                 <i class="bi bi-exclamation-triangle-fill"></i>
-                <div class="p-2">{{ mensagem}}</div>
+                <div class="p-2">{{ mensagem }}</div>
             </div>
             <div class="mb-3 d-flex justify-content-end">
 
-                <button
-                class="btn btn-primary m-2"
-                type="submit"
-                v-on:click.prevent="salvarProduto"
-                >
+                <button class="btn btn-primary m-2" type="submit" v-on:click.prevent="salvarProduto">
                     <i class="bi bi-clipboard2-check"></i>
                     {{ getAcao }}
                 </button>
 
-                <button
-                class="btn btn-warning m-2"
-                type="submit"
-                v-on:click.prevent="cancelar"
-                >
+                <button class="btn btn-warning m-2" type="submit" v-on:click.prevent="cancelar">
                     <i class="bi bi-clipboard2-x"></i>
                     Cancelar
                 </button>
@@ -97,6 +64,9 @@ export default {
         return {
             id: "",
             descricao: "",
+            quantidadeEstoque: 0,
+            precoUnidadeAtual: 0,
+            ativo: "",
             urlImagem: "",
             isInvalido: false,
             mensagem: "",
@@ -110,65 +80,77 @@ export default {
                 return;
             }
             this.isInvalido = false;
-            
+
             try {
                 if (this.id === "") {
                     const response = await produtoService.criar(this.getDados());
                     this.listaProdutos = response.data;
                 } else {
                     const response = await produtoService.atualizar(
-                    this.id,
-                    this.getDados()
-                );
-                this.listaEstados = response.data;
+                        this.id,
+                        this.getDados()
+                    );
+                    this.listaProdutos = response.data;
+                }
+                this.$emit("salvar_produto", {
+                    id: this.id,
+                    descricao: this.descricao,
+                    quantidadeEstoque: this.quantidadeEstoque,
+                    precoUnidadeAtual: this.precoUnidadeAtual,
+                    ativo: this.ativo
+                });
+                this.id = "";
+                this.descricao = "";
+                this.quantidadeEstoque = "";
+                this.precoUnidadeAtual = "";
+                this.ativo = "";
+            } catch (error) {
+                this.isInvalido = true;
+
+                if (error.response.status === 403) {
+                    this.mensagem = "Usuário não identificado! Faça o login!!!";
+                } else if (error.response.status === 400 &&
+                    error.response.data.exception === "ProdutoDuplicadoException") {
+                    this.mensagem = error.response.data.mensagem;
+                } else {
+                    this.mensagem = error.message;
+                }
             }
-            this.$emit("salvar_produto", {
-                id: this.id,
-                descricao: this.descricao, 
-                quantidadeEstoque: this.quantidadeEstoque, 
-                precoUnidadeAtual: this.precoUnidadeAtual, 
-                ativo: this.ativo
-            });
-            this.id = "";
-            this.descricao = "";
-            this.quantidadeEstoque = "";
-            this.precoUnidadeAtual = ""; 
-            this.ativo = "";
-        } catch (error) {this.isInvalido = true;
-            
-            if (error.response.status === 403) {
-              this.mensagem = "Usuário não identificado! Faça o login!!!";
-            } else if ( error.response.status === 400 && 
-                        error.response.data.exception === "ProdutoDuplicadoException") {
-              this.mensagem = error.response.data.mensagem;
-            } else {
-              this.mensagem = error.message;
-            }
-        }
-        
-    },
+
+        },
+
+        getDados() {
+                return { id:this.id,           
+                descricao:this.descricao,
+                urlImagem:this.urlImagem,
+                quantidadeEstoque:this.quantidadeEstoque,
+                precoUnidadeAtual:this.precoUnidadeAtual,
+                ativo:this.ativo };
+
+        },
+
         cancelar() {
             this.id = "";
             this.descricao = "";
             this.urlImagem = "";
             this.quantidadeEstoque = "";
-            this.precoUnidadeAtual = ""; 
+            this.precoUnidadeAtual = "";
             this.ativo = "";
             this.$emit("cancelar", true);
         },
     },
-    mounted(){
+    mounted() {
         if (this.propsProduto) {
             this.id = this.propsProduto.id;
             this.urlImagem = this.propsProduto.urlImagem;
             this.descricao = this.propsProduto.descricao;
             this.quantidadeEstoque = this.propsProduto.quantidadeEstoque;
-            this.precoUnidadeAtual = this.propsProduto.precoUnidadeAtual; 
+            this.precoUnidadeAtual = this.propsProduto.precoUnidadeAtual;
             this.ativo = this.propsProduto.ativo;
         }
     },
     computed: {
-        getAcao(){
+        getAcao() {
             return this.id === "" ? "Incluir" : "Alterar";
         },
     }
