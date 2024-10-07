0f6709cc2c4a7a4cc8e01c340fe27f18921756cc
Enrico da Laqua <89223098+EnricoLaqua@users.noreply.github.com>
2024-10-07 20:20:36 -0300

    Update: App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,12 +11,14 @@ import { RouterLink, RouterView } from "vue-router";
       <RouterLink to="/about">Sobre</RouterLink>
       <RouterLink to="/estados">Estados</RouterLink>
       <RouterLink to="/municipios">Municípios</RouterLink>
-      <RouterLink to="/produtos">Produtos</RouterLink>
       <RouterLink to="/formas-pagamento">Formas de Pagamento</RouterLink>
       <RouterLink to="/clientes">Clientes</RouterLink>
+      <RouterLink to="/produtos">Produtos</RouterLink>
       <RouterLink to="/pedidos">Pedidos</RouterLink>
-      <RouterLink to="/login" v-if="!isLogado">Login</RouterLink>
-      <RouterLink to="/logout" v-if="isLogado">Usuário: {{ login }}</RouterLink>
+      <RouterLink to="/itens-pedido">Itens de Pedidos</RouterLink>
+      <RouterLink to="/login" v-if="!isLogado" class="login-link">Login</RouterLink>
+      <RouterLink to="/logout" v-if="isLogado" class="user-link">Usuário: {{ login }}</RouterLink>
+
      
     </nav>
     <img src="./assets/carrinho.png" alt=""/>
@@ -55,8 +57,8 @@ export default {
 
 img{
   width: 150px;
-  height: 15s0px;
-  margin: auto;
+  height: 150px;
+  margin-right: -90px;
   display: inline-block;
 
 }
@@ -69,14 +71,13 @@ nav {
     background: #23232e;
     border-radius: 11px;
     display: inline-block;
-    width: 80%;
+    width: 90%;
   }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
-
 
 .wrapper nav {
   width: 100%;
@@ -109,5 +110,23 @@ nav a:first-of-type {
   .logo {
     margin: 0 2rem 0 0;
   }
+}
+
+.login-link {
+  color:#ffae00; 
+  font-weight: bold; 
+}
+
+.login-link:hover {
+  color: #ffae00; 
+}
+
+.user-link {
+  color: #198754; 
+  font-weight: bold; 
+}
+
+.user-link:hover {
+  color: #198754; 
 }
 </style> 
