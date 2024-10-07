fde767d8c71657af817abb10aeb106bcd93fa148
Maressa Albuquerque Cortelete <maressacortelete@gmail.com>
2024-10-07 20:06:35 -0300

    correção 2

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ProdutoRepository.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/repository/ProdutoRepository.java
@@ -3,6 +3,7 @@ package br.com.itilh.bdpedidos.sistemapedidos.repository;
 import java.math.BigInteger;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -15,7 +16,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, BigInteger> {
     List<Produto> findByDescricao(String descricao);
     List<Produto> findByDescricaoStartingWithIgnoreCase(String descricao);
     List<Produto> findByDescricaoEndingWithIgnoreCase(String descricao);
-    List<Produto> findByDescricaoContainingIgnoreCase(Pageable pageable, String descricao);
+    Page<Produto> findByDescricaoContainingIgnoreCase(Pageable pageable, String descricao);
 
 
     boolean existsByDescricao (String descricao);
