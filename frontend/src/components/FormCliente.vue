d9201aa3a1224670ccca232d64d2c7ff9832940b
Gustavo201991 <gustavoruppneto@gmail.com>
2024-10-08 17:36:40 -0300

    update_form_listar_cliente

--- frontend/src/components/FormCliente.vue
+++ frontend/src/components/FormCliente.vue
@@ -1,28 +1,78 @@
 <template>
   <div class="container">
-    <h4 class="p-1 mb-1 bg-success text-white">{{ getAcao }} cliente</h4>
+    <h4 class="p-1 mb-1 bg-success text-white">{{ getAcao }} Cliente</h4>
     <hr />
-    <form>
+    <form @submit.prevent="salvarCliente">
       <div class="mb-3">
         <label class="form-label">Id</label>
-        <input class="form-control" type="text" v-model="id" :disabled="true" placeholder="Id cliente" />
+        <input class="form-control" type="text" v-model="id" disabled placeholder="ID Cliente" />
       </div>
       <div class="mb-3">
         <label class="form-label">Nome</label>
-        <input class="form-control" type="text" v-model="nome" placeholder="Nome" />
+        <input class="form-control" type="text" v-model="nomeRazaoSocial" placeholder="Nome/Razao Social" required />
       </div>
+      <div class="mb-3">
+        <label class="form-label">CNPJ</label>
+        <input class="form-control" type="text" v-model="cnpj" placeholder="CNPJ" required />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">CPF</label>
+        <input class="form-control" type="text" v-model="cpf" placeholder="CPF" required />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Telefone</label>
+        <input class="form-control" type="text" v-model="telefone" placeholder="Telefone" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Endereço</label>
+        <input class="form-control" type="text" v-model="endereco" placeholder="Endereço" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Bairro</label>
+        <input class="form-control" type="text" v-model="bairro" placeholder="Bairro" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">CEP</label>
+        <input class="form-control" type="text" v-model="cep" placeholder="CEP" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input class="form-control" type="email" v-model="email" placeholder="Email" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Ativo</label>
+        <select v-model="ativo" class="form-select">
+          <option :value="true">Sim</option>
+          <option :value="false">Não</option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Informação</label>
+        <input class="form-control" type="text" v-model="informacoes" placeholder="Informações" />
+      </div>
+      <label class="form-label">Municipio</label>
+      <v-select class="form-control" label="Produto" :filterable="false"
+          v-model="municipioSelecionado" :options="municipios" @search="onSearchMunicipios">
+          <template v-slot:no-options>
+            Sem Municipios para exibir.
+          </template>
+          <template v-slot:option="option" > 
+              {{ option.nome }}
+          </template>
+          <template v-slot:selected-option="option" >
+              {{ option.nome }}
+          </template>
+      </v-select>
       <div v-if="isInvalido" class="alert alert-danger d-flex align-items-center" role="alert">
         <i class="bi bi-exclamation-triangle-fill"></i>
         <div class="p-2">{{ mensagem }}</div>
       </div>
       <div class="mb-3 d-flex justify-content-end">
-        <button class="btn btn-primary m-2" type="submit" v-on:click.prevent="salvarcliente">
-          <i class="bi bi-clipboard2-check"></i>
-          {{ getAcao }}
+        <button class="btn btn-primary m-2" type="submit">
+          <i class="bi bi-clipboard2-check"></i> {{ getAcao }}
         </button>
-        <button class="btn btn-warning m-2" type="submit" v-on:click.prevent="cancelar">
-          <i class="bi bi-clipboard2-x"></i>
-          Cancelar
+        <button class="btn btn-warning m-2" type="button" @click="cancelar">
+          <i class="bi bi-clipboard2-x"></i> Cancelar
         </button>
       </div>
     </form>
@@ -31,79 +81,119 @@
 
 <script>
 import clienteService from '@/services/clienteService';
+import municipioService from '@/services/municipioService';
+
 export default {
   props: {
-    propscliente: Object,
+    propsCliente: Object,
   },
   data() {
     return {
       id: "",
-      nome: "",
+      nomeRazaoSocial: "",
+      cnpj: "",
+      cpf: "",
+      telefone: "",
+      endereco: "",
+      bairro: "",
+      cep: "",
+      email: "",
+      ativo: false,
+      informacoes: "",
+      municipioSelecionado: null,
+      municipios: [],
       isInvalido: false,
-      mensagem : '',
+      mensagem: '',
     };
   },
   methods: {
-    getDados(){
+    getDados() {
       return {
-              id: this.id,
-              nome: this.nome,
-            };
+        id: this.id,
+        nomeRazaoSocial: this.nomeRazaoSocial,
+        cnpj: this.cnpj,
+        cpf: this.cpf,
+        telefone: this.telefone,
+        endereco: this.endereco,
+        bairro: this.bairro,
+        cep: this.cep,
+        email: this.email,
+        ativo: this.ativo,
+        informacao: this.informacoes,
+        municipioNome: this.municipioNome,
+      };
     },
-    async salvarcliente() {
-      if (this.nome === "") {
+    async onSearchMunicipios(search, loading) {
+      if(search.length) {
+        loading(true);
+        await municipioService.buscar(search).then((response) => {
+        console.log(response);
+        this.municipios = response.content;
+        loading(false);
+      });
+      }
+    },
+    
+    async salvarCliente() {
+      if (!this.nomeRazaoSocial || !this.cpf || !this.cnpj) {
         this.isInvalido = true;
-        this.mensagem = "Nome deve ser preenchido!!";
+        this.mensagem = "Nome, CPF e CNPJ devem ser preenchidos!";
         return;
       }
       this.isInvalido = false;
-      
-    try{
-        if (this.id === "") {
-          const response = await clienteService.criar(
-            this.getDados());
-          this.listaclientes = response;
-        } else {
-          const response = await clienteService.atualizar(
-            this.id,
-            this.getDados()
-          );
-          this.listaclientes = response;
-        }
-        this.$emit("salvar_cliente", {
-        id: this.id,
-        nome: this.nome,
-      });
 
-      this.id = "";
-      this.nome = "";
-    }catch(error){
-      this.isInvalido = true;
-      if(error.response.status === 403){        
-        this.mensagem = "Usuário não identificado! Faça o login!!!";
-      }else if(error.response.status === 400 &&
-               error.response.data.exception === 'clienteDuplicadoException'){
-        this.mensagem = error.response.data.mensagem;     
-      }else{
-        this.mensagem = error.message;
+      try {
+        let response;
+        if (!this.id) {
+          response = await clienteService.criar(this.getDados());
+        } else {
+          response = await clienteService.atualizar(this.id, this.getDados());
+        }
+        this.$emit("salvar_cliente", response);
+        this.limparCampos();
+      } catch (error) {
+        this.isInvalido = true;
+        this.mensagem = this.getErrorMessage(error);
       }
-    }
-   },
+    },
+    getErrorMessage(error) {
+      if (error.response) {
+        if (error.response.status === 403) {
+          return "Usuário não identificado! Faça o login!";
+        } else if (error.response.status === 400 && error.response.data.exception === 'ClienteDuplicadoException') {
+          return error.response.data.mensagem;
+        }
+      }
+      return error.message || "Ocorreu um erro inesperado.";
+    },
     cancelar() {
-      this.id = "";
-      this.nome = "";
+      this.limparCampos();
       this.$emit("cancelar", true);
     },
+    limparCampos() {
+      this.id = "";
+      this.nomeRazaoSocial = "";
+      this.cnpj = "";
+      this.cpf = "";
+      this.telefone = "";
+      this.endereco = "";
+      this.bairro = "";
+      this.cep = "";
+      this.email = "";
+      this.ativo = false;
+      this.informacoes = "";
+      this.municipioNome = "";
+      this.municipioSelecionado = null;
+    }
   },
   mounted() {
-    if (this.propscliente) {
-      this.id = this.propscliente.id;
-      this.nome = this.propscliente.nome;
+    if (this.propsCliente) {
+      Object.assign(this, this.propsCliente);
     }
   },
   computed: {
     getAcao() {
-      return this.id === "" ? "Incluir" : "Alterar";
+      return this.id ? "Alterar" : "Incluir";
     },
   },
 };
--- frontend/src/components/ListarCliente.vue
+++ frontend/src/components/ListarCliente.vue
@@ -1,208 +1,215 @@
 <template>
-    <div class="container">
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+        <h3>CLIENTES</h3>
+      </div>
+      <div class="col-2 d-flex justify-content-end">
+        <button v-if="!formVisible" @click="novoCliente" class="btn btn-success">
+          <i class="bi bi-clipboard-plus"></i> Novo
+        </button>
+      </div>
       <div class="row">
-        <div class="col-10">
-          <h3>CLIENTE</h3>
+        <div>
+          <FormCliente
+            v-if="formVisible"
+            :propsCliente="clienteEscolhido"
+            @cancelar="limpar"
+            @salvar_cliente="buscarClientes"
+          />
         </div>
-        <div class="col-2 d-flex justify-content-end">
-          <button v-if="!formVisible" @click="novoCliente" class="btn btn-success">
-            <i class="bi bi-clipboard-plus"></i> Novo
+      </div>
+    </div>
+
+    <table class="table table-dark table-striped" v-if="!formVisible">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nome/Razão Social</th>
+          <th scope="col">Telefone</th>
+          <th scope="col">Endereço</th>
+          <th scope="col">CNPJ</th>
+          <th scope="col">Bairro</th>
+           <th scope="col">CEP</th>
+           <th scope="col">Email</th>
+           <th scope="col">Ativo</th>
+           <th scope="col">CPF</th>
+           
+
+
+          <th scope="col" class="d-flex justify-content-end">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="cliente in listaClientes" :key="cliente.id" scope="row">
+          <th>
+            {{ cliente.id }}
+          </th>
+          <th>
+            {{ cliente.nomeRazaoSocial }}
+          </th>
+          <th>
+            {{ cliente.telefone }}
+          </th>
+          <td>
+            {{ cliente.endereco }}
+          </td>
+          <td>
+            {{ cliente.cnpj }}
+          </td>
+          <td>
+            {{ cliente.endereco }}
+          </td>
+          <td>
+            {{ cliente.cep }}
+          </td>
+          <td>
+            {{ cliente.email }}
+          </td>
+          <td>
+            {{ cliente.ativo }}
+          </td>
+          <td>
+            {{ cliente.cpf }}
+          </td>
+       
+          <td class="d-flex justify-content-end">
+            <button
+              class="btn btn-btn btn-primary m-2"
+              @click="alterarCliente(cliente)"
+            >
+              <i class="bi bi-clipboard-pulse"></i> Alterar
+            </button>
+
+            <button
+              class="btn btn-outline-danger m-2"
+              @click="excluirCliente(cliente.id)"
+            >
+              <i class="bi bi-clipboard2-minus"></i> Excluir
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div v-if="!formVisible">
+    <hr />
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-auto">
+          <button
+            v-for="pagina in totalPages"
+            :key="pagina"
+            @click.prevent="irPara(pagina)"
+            class="btn btn-light ms-1"
+          >
+            {{ pagina }}
           </button>
         </div>
-        <div class="row">
-          <div>
-            <FormProduto
-              v-if="formVisible"
-              :propsCliente="clienteEscolhido"
-              @cancelar="limpar"
-              @salvar_cliente="buscarCliente"
-            />
-          </div>
+        <div class="col-auto">
+          <input
+            type="text"
+            v-model="pageNumber"
+            placeholder="Número da página"
+            class="form-control w-25"
+          />
         </div>
-      </div>
-  
-      <table class="table table-dark table-striped" v-if="!formVisible">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Imagen</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Quantidade</th>
-            <th scope="col">Preço</th>
-            <th scope="col">Ativo</th>
-            <th scope="col" class="d-flex justify-content-end">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="produto in listaProdutos" :key="produto.id" scope="row">
-            <th>
-              {{ produto.id }}
-            </th>
-            <td>
-              <img :src=produto.urlImagem height="100px">
-            </td>
-            <td>
-              {{ produto.descricao }}
-            </td>
-            <td>
-              {{ formatarQuantidade(produto.quantidadeEstoque) }}
-            </td>
-            <td>
-              {{ formatarPreco(produto.precoUnidadeAtual) }}
-            </td>
-            <td>
-              {{ formatarLogico(produto.ativo) }}
-            </td>
-            <td class="d-flex justify-content-end">
-              <button
-                class="btn btn-btn btn-primary m-2"
-                @click="alterarProduto(produto)"
-              >
-                <i class="bi bi-clipboard-pulse"></i> Alterar
-              </button>
-  
-              <button
-                class="btn btn-outline-danger m-2"
-                @click="excluirProduto(produto.id)"
-              >
-                <i class="bi bi-clipboard2-minus"></i> Excluir
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div v-if="!formVisible">
-      <hr />
-      <div class="container">
-        <div class="row d-flex justify-content-center">
-          <div class="col-auto">
-  
-            <button
-              v-for="pagina in totalPages"
-              :key="pagina"
-              @click.prevent="irPara(pagina)"
-              class="btn btn-light ms-1"
-            >
-              {{ pagina }}
-            </button>
-  
-  
-          </div>
-          <div class="col-auto">
-            <input
-              type="text"
-              v-model="pageNumber"
-              placeholder="Número da pagina"
-              class="form-control w-25"
-            />
-          </div>
-          <div class="col-auto">
-            <select v-model="pageSize" class="form-select">
-              <option value="2">2</option>
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="50">50</option>
-            </select>
-          </div>
-          <div class="col-auto">
-            <select v-model="property" class="form-select">
-              <option value="id">ID</option>
-              <option value="descricao">Descrição</option>
-            </select>
-          </div>
-          <div class="col-auto">
-            <select v-model="direction" class="form-select">
-              <option value="ASC">Crescente</option>
-              <option value="DESC">Decrescente</option>
-            </select>
-          </div>
-          <div class="col-auto">
-            <button @click.prevent="buscarClientes" class="btn btn-success">
-              <i class="bi bi-binoculars"></i>
-              Buscar
-            </button>
-          </div>
+        <div class="col-auto">
+          <select v-model="pageSize" class="form-select">
+            <option value="2">2</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+          </select>
+        </div>
+        <div class="col-auto">
+          <select v-model="property" class="form-select">
+            <option value="id">ID</option>
+            <option value="nomeRazaoSocial">Nome/Razão Social</option>
+          </select>
+        </div>
+        <div class="col-auto">
+          <select v-model="direction" class="form-select">
+            <option value="ASC">Crescente</option>
+            <option value="DESC">Decrescente</option>
+          </select>
+        </div>
+        <div class="col-auto">
+          <button @click.prevent="buscarClientes" class="btn btn-success">
+            <i class="bi bi-binoculars"></i> Buscar
+          </button>
         </div>
       </div>
     </div>
-  </template>
-  
-  
-  <script>
-  import FormCliente from "./FormCliente.vue";
-  import Logico from "@/utils/Logico.js";
-  import Monetario from "@/utils/Monetario.js";
-  import Decimal from "@/utils/Decimal.js";
-  import axios from "axios";
-  export default {
-    components: {
-      FormCliente,
+  </div>
+</template>
+
+<script>
+import FormCliente from "./FormCliente.vue";
+import clienteService from "@/services/clienteService";
+
+export default {
+  components: {
+    FormCliente,
+  },
+  data() {
+    return {
+      listaClientes: [],
+      clienteEscolhido: null,
+      formVisible: false,
+      pageNumber: 1,
+      pageSize: 10,
+      direction: "ASC",
+      property: "id",
+      totalPages: 0,
+    };
+  },
+  methods: {
+    async buscarClientes() {
+      this.clienteEscolhido = null;
+      this.formVisible = false;
+      const response = await clienteService.listar(
+        this.pageNumber,
+        this.pageSize,
+        this.direction,
+        this.property
+      );
+      this.listaClientes = response.content;
+      this.totalPages = response.totalPages;
     },
-    data() {
-      return {
-        listaCliente: [],
-        clienteEscolhido: null,
-        formVisible: false,
-        pageNumber: 1,
-        pageSize: 10,
-        direction: "ASC",
-        property: "id",
-        totalPages: 0,
-      };
+    limpar() {
+      this.clienteEscolhido = null;
+      this.formVisible = !this.formVisible;
     },
-    methods: {
-      async buscarClientes() {
-        this.produtoEscolhido = null;
-        this.formVisible = false;
-  
-      
-        const response = await axios.get(
-          `http://localhost:8080/clientes?pageNumber=${this.pageNumber}&pageSize=${this.pageSize}&direction=${this.direction}&property=${this.property}`
-        );
-        console.log(response.data);
-        this.listaProdutos = response.data.content;
-        this.totalPages = response.data.totalPages;
-        console.log(this.totalPages);
-      },
-      limpar() {
-        this.produtoEscolhido = null;
-        this.formVisible = !this.formVisible;
-      },
-      novoProduto() {
-        this.formVisible = !this.formVisible;
-      },
-      alterarProduto(produto) {
-        this.produtoEscolhido = produto;
-        this.formVisible = true;
-      },
-      async excluirProduto(id) {
-        let config = {
-        headers: {
-          'Authorization': 'Bearer ' +localStorage.getItem('token')
-          }
+    novoCliente() {
+      this.formVisible = !this.formVisible;
+    },
+    alterarCliente(cliente) {
+      this.clienteEscolhido = cliente;
+      this.formVisible = true;
+    },
+    async excluirCliente(id) {
+      try {
+        const response = await clienteService.apagar(id);
+        console.log(response);
+      } catch (error) {
+        if (error.response.status === 403) {
+          alert("Usuário não identificado! Faça o login!!!");
+        } else if (error.response.status === 400) {
+          alert(error.response.data.mensagem);
+        } else {
+          alert(error.message);
         }
-        const response = await axios.delete(`http://localhost:8080/produto/${id}`,config);
-        console.log(response.data);
-        this.buscarProdutos();
-      },
-      irPara(pagina) {
-        this.pageNumber = pagina;
-        this.buscarProdutos();
-      },
-      formatarLogico(valor){
-        return Logico.toSimNao(valor);
-      },
-      formatarPreco(valor){
-        return Monetario.toTela(valor);
-      },
-      formatarQuantidade(valor){
-        return Decimal.toTela(valor);
       }
+      this.buscarClientes();
     },
-    mounted() {
-      this.buscarProdutos();
+    irPara(pagina) {
+      this.pageNumber = pagina;
+      this.buscarClientes();
     },
-  };
-  </script>
-  
+  },
+  mounted() {
+    this.buscarClientes();
+  },
+};
+</script>
