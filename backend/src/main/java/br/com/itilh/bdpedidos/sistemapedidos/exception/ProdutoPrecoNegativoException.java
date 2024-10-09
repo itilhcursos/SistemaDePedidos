4fa80ef589beb3374a6d35fe67df4c5cbb518731
Guilherme Vilaruel <guilhermevillaruel@gmail.com>
2024-10-08 23:20:06 -0300

    UpdateException

---
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/exception/ProdutoPrecoNegativoException.java
@@ -0,0 +1,12 @@
+package br.com.itilh.bdpedidos.sistemapedidos.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ProdutoPrecoNegativoException extends RuntimeException{
+
+    public ProdutoPrecoNegativoException(String produto){
+        super("O produto :" + produto + " não pode ter preço negativo.");
+    }
+}
