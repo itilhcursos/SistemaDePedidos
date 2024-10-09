901f0476dc5dc6198ec9702af5f9692af6930192
Guilherme Vilaruel <guilhermevillaruel@gmail.com>
2024-10-08 21:04:32 -0300

    update ItemPedidoController

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/ItemPedidoController.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/ItemPedidoController.java
@@ -1,5 +1,56 @@
 package br.com.itilh.bdpedidos.sistemapedidos.controller;
 
+import java.math.BigInteger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.itilh.bdpedidos.sistemapedidos.dto.ClienteDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.dto.FormaPagamentoDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.dto.ItemPedidoDTO;
+import br.com.itilh.bdpedidos.sistemapedidos.service.ItemPedidoService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+@RestController
+@SecurityRequirement(name = "bearerAuth")
 public class ItemPedidoController {
+    @Autowired
+    ItemPedidoService itemPedidoService;
+
+
+     @GetMapping("/itens-pedido")
+    public Page<ItemPedidoDTO> getTodos(
+        @RequestParam(required = false, defaultValue = "1") int pageNumber,
+        @RequestParam(required = false, defaultValue = "10") int pageSize,
+        @RequestParam(required = false, defaultValue = "ASC") String direction,
+        @RequestParam(required = false, defaultValue = "id") String property
+    ) {
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, Sort.Direction.valueOf(direction), property);
+        return itemPedidoService.listarItensPedido(pageable);
+
+    }
+
+
+    @GetMapping("/Item-pedido/{id}")
+    public ItemPedidoDTO getPorId(@PathVariable BigInteger id) throws Exception {
+        return itemPedidoService.buscarItemPedidoPorId(id);
+
+    }
+
+    @PostMapping("/Item-pedido/{id}")
+    public ItemPedidoDTO
     
-}
+
+
+    }
