6eda9294e2ae2d0d5dc3025b0461a4beb75504fd
Guilherme Vilaruel <guilhermevillaruel@gmail.com>
2024-10-11 20:53:17 -0300

    update_exception

---
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/exception/FormaPagamentoDuplicadoException.java
@@ -0,0 +1,15 @@
+package br.com.itilh.bdpedidos.sistemapedidos.exception;
+
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+public class FormaPagamentoDuplicadoException extends RuntimeException{
+
+    public FormaPagamentoDuplicadoException(String nomeEstado){
+        super("O estado :" + nomeEstado + " já existe");
+    }
+}
+
