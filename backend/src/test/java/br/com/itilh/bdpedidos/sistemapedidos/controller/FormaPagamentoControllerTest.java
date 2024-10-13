3aaf4068f7916be2cd5eda64387523281c745caa
Vitor Paulo da Penha <vitorpaulodapenha03@gmail.com>
2024-10-13 08:32:55 -0300

    Update: Configuração controller de pedido e Item pedido

--- backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/FormaPagamentoControllerTest.java
+++ backend/src/test/java/br/com/itilh/bdpedidos/sistemapedidos/controller/FormaPagamentoControllerTest.java
@@ -1,5 +1,42 @@
 package br.com.itilh.bdpedidos.sistemapedidos.controller;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.itilh.bdpedidos.sistemapedidos.repository.FormaPagamentoRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class FormaPagamentoControllerTest {
+
+      @Autowired
+      MockMvc mockMvc;
+
+      @Autowired
+      FormaPagamentoRepository formaPagamentoRepository;
+
+      @Test
+      @DisplayName("Teste do path /formaPagamento")
+      void testGetFormaPagamento() throws Exception{
+        mockMvc.perform(get("/formaPagamento")).andExpect(status().isOk())
+        .andExpect(content().string(containsString("totalElements")));
+      }
+
+      @Test
+      @DisplayName("Teste de path inexistente")
+      void testGetFormaPagamentoInexistente() throws Exception{
+        mockMvc.perform(get("/formaPagamento")).andExpect(status().isMethodNotAllowed());
+      }
 
 }
--- frontend/src/components/ListarPedido.vue
+++ frontend/src/components/ListarPedido.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-10">
-        <h3>Pedidos</h3>
+        <h3>Listagem de Pedidos</h3>
       </div>
       <div class="col-2 d-flex justify-content-end">
         <button v-if="!formVisible" @click="novo" class="btn btn-success">
