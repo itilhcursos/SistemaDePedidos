e1350bc4fec064f862d6b678ee92d5f086fe5db5
Matteo Miguel Rhein <matteo2005rh@gmail.com>
2024-11-25 17:40:25 -0300

    corrigindo

--- backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/security/SecurityConfigurations.java
+++ backend/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/security/SecurityConfigurations.java
@@ -4,24 +4,24 @@ package br.com.itilh.bdpedidos.sistemapedidos.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import br.com.itilh.bdpedidos.sistemapedidos.controller.AuthenticationController;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
-
+@EnableWebSecurity
 @OpenAPIDefinition(info = @Info(title = "Sistema de Pedidos", version = "v1"))
 @SecurityScheme(
     name = "bearerAuth",
@@ -35,15 +35,15 @@ public class SecurityConfigurations {
     SecurityFilter securityFilter;
 
     @Bean
-    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.csrf(csrf-> csrf.disable())
         .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests( authorize -> authorize                        
                         .requestMatchers(HttpMethod.POST, "/auth/login").anonymous()
                         .requestMatchers(HttpMethod.POST, "/auth/registro").anonymous()
-                        .requestMatchers(HttpMethod.POST, "/").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
                         )
                         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
@@ -51,7 +51,7 @@ public class SecurityConfigurations {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager (AuthenticationController authenticationConfiguration) throws Exception{
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
     }
 
